f30412ea0f5e618c34cf5cb2f74aa639154cf0db
iGioRojas <geovanny_rl14@hotmail.com>
2024-03-23 12:21:28 -0500

    Update NavBar

--- src/shared/NavBar.jsx
+++ src/shared/NavBar.jsx
@@ -8,6 +8,10 @@ function Navbar() {
         <li><Link to="/conferencistas">Conferencistas</Link></li>
         <li><Link to="/cronograma">Cronograma</Link></li>
         <li><Link to="/eventos">Eventos</Link></li>
+        <ul>
+          <li><Link to="/eventos">Wids</Link></li>
+          <li><Link to="/eventos">Datathon</Link></li>
+        </ul>
         <li><Link to="/nosotros">Nosotros</Link></li>
       </ul>
     </nav>
