2d7e66b21bc55f3066647cb20251272d1d5f65de
Braulio Rivas Abad <brauliorivas2004@gmail.com>
2024-03-27 19:20:45 -0500

    seccion de sponsors #6

--- src/pages/Home/Inicio.jsx
+++ src/pages/Home/Inicio.jsx
@@ -1,10 +1,13 @@
-import {Carrusel} from "./Components/Carrusel";
+import { Carrusel } from "./Components/Carrusel";
+import Sponsors from "./Components/Sponsors";
 
 function Inicio() {
     return (
-        <h1>INICIO</h1>,
-        <Carrusel></Carrusel>
-
+        <>
+            <h1>INICIO</h1>
+            <Carrusel></Carrusel>
+            <Sponsors/>
+        </>
     )
 }
 
