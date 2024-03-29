64263c648536e876517c5196decc44f23b5429df
Luis R. <lromeror@fiec.espol.edu.ec>
2024-03-28 19:28:49 -0500

    Ubicacion listo falta cronograma

--- src/pages/Home/Inicio.jsx
+++ src/pages/Home/Inicio.jsx
@@ -1,9 +1,11 @@
 import {Carrusel} from "./Components/Carrusel";
+import Ubicacion from "./Components/Ubicacion";
 
 function Inicio() {
     return (
         <h1>INICIO</h1>,
-        <Carrusel></Carrusel>
+        <Carrusel></Carrusel>,
+        <Ubicacion></Ubicacion>
 
     )
 }
