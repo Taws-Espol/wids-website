9cdf7f18314cd552fca4a5c7ced09df24cb92dff
Luis R. <lromeror@fiec.espol.edu.ec>
2024-04-01 07:50:30 -0500

    Cronograma Necsita ajustes

--- src/pages/Home/Inicio.jsx
+++ src/pages/Home/Inicio.jsx
@@ -1,11 +1,20 @@
 import {Carrusel} from "./Components/Carrusel";
+import Cronograma from "./Components/Cronograma";
 import Ubicacion from "./Components/Ubicacion";
+
 
 function Inicio() {
     return (
-        <h1>INICIO</h1>,
+        <div>
+            <h1>INICIO</h1>,
         <Carrusel></Carrusel>,
+        
+        <Cronograma></Cronograma>,
         <Ubicacion></Ubicacion>
+        
+        </div>
+        
+        
 
     )
 }
