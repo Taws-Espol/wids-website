09dbf1af5d6b9e9539c8ce93ca5943a9f9d6ff31
Roberto Patiño <rapatino@espol.edu.ec>
2024-04-10 21:00:13 -0500

    Fix paths

--- src/pages/Home/Components/Sponsors.jsx
+++ src/pages/Home/Components/Sponsors.jsx
@@ -1,20 +1,20 @@
-import { sponsors } from "./sponsors"
-import Sponsor from "./Sponsor"
-import Subtitle from "./Subtitle"
+import { sponsors } from "../../../data/sponsors";
+import Sponsor from "./Sponsor";
+import Subtitle from "./Subtitle";
 
 export default function Sponsors() {
-    return (
-        <div className="flex flex-col items-center p-20 ">
-            <Subtitle text="Organizador" />
-            <div className="my-20">
-                <img src="src/assets/taws.png" alt="Taws" width={200} />
-            </div>
-            <Subtitle text="Sponsors" />
-            <div className="grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 gap-y-12 gap-x-10">
-                {sponsors.map((sponsor, index) => (
-                    <Sponsor key={index} {...sponsor} />
-                ))}
-            </div>
-        </div>
-    )
+	return (
+		<div className='flex flex-col items-center p-20 '>
+			<Subtitle text='Organizador' />
+			<div className='my-20'>
+				<img src='src/assets/taws.png' alt='Taws' width={200} />
+			</div>
+			<Subtitle text='Sponsors' />
+			<div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 gap-y-12 gap-x-10'>
+				{sponsors.map((sponsor, index) => (
+					<Sponsor key={index} {...sponsor} />
+				))}
+			</div>
+		</div>
+	);
 }
--- src/pages/Home/Inicio.jsx
+++ src/pages/Home/Inicio.jsx
@@ -1,38 +1,42 @@
-import {Carrusel} from "./Components/Carrusel";
-import InfoCarrusel from './Components/info-de-conferencia/Carrusel/Carrusel';
-import Info from './Components/info-de-conferencia/Info/Info'
+import { Carrusel } from "./Components/Carrusel";
+// import InfoCarrusel from "./Components/info-de-conferencia/Carrusel/Carrusel";
+import Info from "./components/info-de-conferencia/Info/Info";
 import Sponsors from "./Components/Sponsors";
 import Cronograma from "./Components/Cronograma";
 import Ubicacion from "./Components/Ubicacion";
 
-
 function Inicio() {
-    return (
-        <>
-            <Carrusel/>
-            <section>
-                <div>
-                    <h1 className="w-full text-center text-3xl font-sans font-bold" style={{color: '#004029'}}>
-                        Conferencia
-                    </h1>
-                    <h2 className="w-full text-center text-xl font-mono p-5 font-semibold ">
-                        13 de Julio, 2024 · Guayaquil - Espol
-                    </h2>
-                    <Info/>
-                </div>
-                <div className='mt-14' style={{color: 'white', backgroundColor: 'black'}}>
-                    <h1 className="p-10 text-center text-3xl font-serif font-bold">
-                        Conferencistas
-                    </h1>
-                    <InfoCarrusel/>
-                </div>
-            </section>
-            <Sponsors></Sponsors>,
-            <Cronograma/>,
-            <Ubicacion/>
-        </>
-    );
-
+	return (
+		<>
+			<Carrusel />
+			<section>
+				<div>
+					<h1
+						className='w-full text-center text-3xl font-sans font-bold'
+						style={{ color: "#004029" }}
+					>
+						Conferencia
+					</h1>
+					<h2 className='w-full text-center text-xl font-mono p-5 font-semibold '>
+						13 de Julio, 2024 · Guayaquil - Espol
+					</h2>
+					<Info />
+				</div>
+				<div
+					className='mt-14'
+					style={{ color: "white", backgroundColor: "black" }}
+				>
+					<h1 className='p-10 text-center text-3xl font-serif font-bold'>
+						Conferencistas
+					</h1>
+					{/* <InfoCarrusel /> */}
+				</div>
+			</section>
+			<Sponsors></Sponsors>,
+			<Cronograma />,
+			<Ubicacion />
+		</>
+	);
 }
 
 export default Inicio;
