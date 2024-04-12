2ed1dd5d76e3de8eef3c7f4099ffeb876227a642
KevinFernandez21 <136862908+KevinFernandez21@users.noreply.github.com>
2024-04-11 23:48:16 -0500

    navbar responsive and ButtonUp #14 #24

--- src/shared/NavBar.jsx
+++ src/shared/NavBar.jsx
@@ -4,8 +4,7 @@ import { Link } from "react-router-dom";
 function Navbar() {
 	const [isOpen, setIsOpen] = useState(false);
 	const [isOpen1, setIsOpen1] = useState(false);
-	const [isEventHovered, setIsEventHovered] = useState(false);
-	isEventHovered;
+	
 
 	const toggle = () => {
 		setIsOpen(!isOpen);
@@ -13,206 +12,159 @@ function Navbar() {
 	const toggle1 = () => {
 		setIsOpen1(!isOpen1);
 	};
-	const handleEventHover = () => {
-		setIsEventHovered(true);
-		const arrowIcon = document.getElementById("arrow-icon");
-		arrowIcon.classList.add("gradient-text");
-	};
-	const handleEventHoverOut = () => {
-		setIsEventHovered(false);
-		const arrowIcon = document.getElementById("arrow-icon");
-		arrowIcon.classList.remove("gradient-text");
-	};
+
 	return (
 		<>
-			<nav
-				className='  xl:flex 
-                      xl:items-center 
-                      xl:justify-between 
-                      xl:text-[20px] 
-                      xl:text-lg 
-                      top-0 left-0 
-                      w-full 
-                      font-acumin 
-                      text-base '
-			>
-				<div
-					className='flex 
-                      items-center 
-                      bg-[rgb(252,252,252)] 
-                      justify-between'
-				>
+			<nav className='    xl:flex 
+								xl:items-center 
+								xl:justify-between 
+								xl:text-[20px] 
+								xl:text-lg 
+								top-0 left-0 
+								w-full 
+								font-acumin 
+								text-base'>
+				<div className='flex 
+                                items-center 
+                                bg-[rgb(252,252,252)] 
+                                justify-between'>
 					<Link to='/'>
 						<img
 							src='/src/assets/navbar/Imagen de WhatsApp 2024-03-25 a las 21.16.47_83686aab.jpg'
-							alt=''
-						/>
+							alt=''/>
 					</Link>
-					<div
-						className=' xl:hidden 
-                          rounded-md 
-                          w-6  
-                          items-center 
-                          mx-6 
-                          hover:bg-gray-100 '
-						onClick={toggle}
-					>
-						<span
-							className={`block 
-                            w-full h-1 
-                            rounded 
-                            bg-black 
-                            transition-transform duration-300 
-                            ${
-															isOpen
-																? "transform rotate-45 translate-y-1.5 -translate-x-0"
-																: "mt-1"
-														}`}
+					<div className='xl:hidden 
+                                    rounded-md 
+                                    w-6  
+                                    items-center 
+                                    mx-6 
+                                    hover:bg-gray-100 '
+						onClick={toggle}>
+						<span className={`  block 
+                                            w-full h-1 
+											rounded 
+											bg-black 
+											transition-transform duration-300 
+                                            ${isOpen ? "transform rotate-45 translate-y-1.5 -translate-x-0": "mt-1"}`}
 						></span>
-						<span
-							className={`block 
-                            w-full h-1 
-                            rounded 
-                            bg-black 
-                            transition-all duration-300 
-                            ${isOpen ? "opacity-0" : "mt-1"}`}
+						<span className={`  block 
+											w-full h-1 
+											rounded 
+											bg-black 
+											transition-all duration-300 
+											${isOpen ? "opacity-0" : "mt-1"}`}
 						></span>
-						<span
-							className={`block 
-                            w-full h-1 
-                            rounded 
-                            bg-black 
-                            transition-transform duration-300 
-                            ${isOpen ? 'transform -rotate-45 -translate-y-0.5 -translate-x-0' : 'mt-1'}`}>
-          </span>
-        </div>
-      </div>
-      <ul className={`xl:w-auto w-full
-                      z-50 
-                      xl:static absolute
-                      xl:flex 
-                      xl:l-0 
-                      xl:bg-none 
-                      p-4 
-                      transition-all duration-600 ease-in
-                      xl:translate-x-0 
-                      bg-gradient-custom 
-                      ${
-												isOpen
-													? "translate-x-0 transition-transform duration-300 ease-in-out"
-													: "translate-x-full transition-transform duration-300 ease-in-out"
-											}`}
-				>
-					<li
-						className=' xl:border-0 border-2 
-                        xl:font-thin font-black 
-                        xl:shadow-none shadow-lg  
-                        xl:mb-0 mb-1'
-					>
-						<Link
-							to='/'
-							className=' xl:px-8 xl:py-8 
-                            xl:text-[20px] text-[25px] 
-                            xl:text-black text-white  
-                            gradient-text-hover gradient-underline-hover
-                            block  
-                            text-center text-stroke  
-                            pt-6 pb-6'
-						>
+						<span className={`  block 
+											w-full h-1 
+											rounded 
+											bg-black 
+											transition-transform duration-300 
+											${isOpen ? 'transform -rotate-45 -translate-y-0.5 -translate-x-0' : 'mt-1'}`}
+						></span>
+                    </div>
+                </div>
+                <ul className={`xl:w-auto w-full
+                                z-50 
+                                xl:static absolute
+                                xl:flex 
+                                xl:l-0 
+                                xl:bg-none 
+                                p-4 
+                                transition-all duration-600 ease-in
+                                xl:translate-x-0 
+                                bg-gradient-custom 
+                                ${isOpen ? "opacity-100": "opacity-0 xl:opacity-100"}`}>
+					<li className=' xl:border-0 border-2 
+                                    xl:font-thin font-black 
+                                    xl:shadow-none shadow-lg  
+                                    xl:mb-0 mb-1'>
+						<Link   to='/'
+                                className=' xl:px-8 xl:py-8 
+                                            xl:text-[20px] text-[25px] 
+                                            xl:text-black text-white  
+											gradient-text-hover gradient-underline-hover
+											block  
+											text-center text-stroke  
+											pt-6 pb-6'>
 							Inicio
 						</Link>
 					</li>
-					<li
-						className=' xl:border-0 border-2
+					<li className=' xl:border-0 border-2
                         xl:font-thin font-black
                         xl:shadow-none shadow-lg 
-                        mb-1'
-					>
-						<Link
-							to='/cronograma'
-							className=' xl:px-8 xl:py-8 
-                            xl:text-[20px] text-[25px]
-                            xl:text-black text-white 
-                            gradient-text-hover gradient-underline-hover
-                            relative
-                            block  
-                            text-center  
-                            pt-6 pb-6'
-						>
+                        mb-1'>
+						<Link   to='/cronograma'
+                                className=' xl:px-8 xl:py-8 
+											xl:text-[20px] text-[25px]
+											xl:text-black text-white 
+											gradient-text-hover gradient-underline-hover
+											relative
+											block  
+											text-center  
+											pt-6 pb-6'>
 							Conferencistas
 						</Link>
 					</li>
-					<li
-						className=' xl:border-0 border-2
+					<li className=' xl:border-0 border-2
                         xl:font-thin font-black
                         xl:shadow-none shadow-lg
-                        mb-1 relative'
-					>
-						<Link
-							to='/cronograma'
-							className='xl:px-8 xl:py-8 
-                          xl:text-[20px] text-[25px]
-                          xl:text-black text-white
-                          gradient-text-hover gradient-underline-hover 
-                          relative
-                          block  
-                          text-center  
-                          pt-6 pb-6'
-						>
+                        mb-1 relative'>
+						<Link   to='/cronograma'
+                                className='xl:px-8 xl:py-8 
+								xl:text-[20px] text-[25px]
+								xl:text-black text-white
+								gradient-text-hover gradient-underline-hover 
+								relative
+								block  
+								text-center  
+								pt-6 pb-6'>
 							Cronograma
 						</Link>
 					</li>
-					<li
-						className=' xl:relative
-                        xl:border-0 border-2
-                        xl:font-thin font-black
-                        xl:shadow-none shadow-lg   
-                        xl:flex 
-                        justify-center 
-                        items-center
-                        mb-1'
-					>
-						<div
-							className='flex 
-          justify-center 
-          items-center'>
-            <p className=' xl:px-8 xl:py-8 
-                              xl:text-[20px] text-[25px]
-                              xl:text-black text-white
-                              gradient-text-hover gradient-underline-hover 
-                              text-center 
-                              pt-6 pb-6   
-                              flex 
-                              justify-center 
-                              items-center
-                              cursor-pointer'
+					<li className=' xl:relative
+									xl:border-0 border-2
+									xl:font-thin font-black
+									xl:shadow-none shadow-lg   
+									xl:flex 
+									justify-center 
+									items-center
+									mb-1'>
+						<div className='flex 
+										justify-center 
+										items-center'>
+                            <p className='  xl:px-8 xl:py-8 
+											xl:text-[20px] text-[25px]
+											xl:text-black text-white
+											gradient-text-hover gradient-underline-hover 
+											text-center 
+											pt-6 pb-6   
+											flex 
+											justify-center 
+											items-center
+											cursor-pointer'
 								id='event-section'
-								onClick={toggle1}
-							>
+								onClick={toggle1}>
 								Eventos
 							</p>
 						</div>
-						<ul
-							className={`xl:absolute 
-                          xl:left-0 
-                          xl:top-full 
-                          xl:-mt-6 
-                          bg-white 
-                          xl:shadow-lg 
-                          text-center 
-                          dropdown-menu 
-                          ${isOpen1 ? "show" : ""}`}
-						>
+						<ul className={`xl:absolute 
+										xl:left-0 
+										xl:top-full 
+										xl:-mt-6 
+										bg-white 
+										xl:shadow-lg 
+										text-center 
+										dropdown-menu 
+										${isOpen1 ? "show" : ""}`}>
 							<li className='link-with-gradient-border '>
 								<Link
 									to='/eventos/wids'
 									className=' gradient-border-bottom 
-                                xl:px-8 xl:py-2 
-                                xl:text-[20px] text-[25px]
-                                block  
-                                hover:bg-gray-100 
-                                pt-5 pb-5'
-								>
+												xl:px-8 xl:py-2 
+												xl:text-[20px] text-[25px]
+												block  
+												hover:bg-gray-100 
+												pt-5 pb-5'>
 									Wids
 								</Link>
 							</li>
@@ -220,51 +172,42 @@ function Navbar() {
 								<Link
 									to='/eventos/datathon'
 									className=' xl:px-8 xl:py-2 
-                                xl:text-[20px] text-[25px] 
-                                block  
-                                hover:bg-gray-100 
-                                pt-5 pb-5'
-								>
+												xl:text-[20px] text-[25px] 
+												block  
+												hover:bg-gray-100 
+												pt-5 pb-5'>
 									Datathon
 								</Link>
 							</li>
 						</ul>
 					</li>
-					<li
-						className=' xl:border-0 border-2
-                        xl:font-thin font-black
-                        xl:shadow-none shadow-lg'
-					>
-						<Link
-							to='/ediciones'
-							className='xl:px-10 xl:py-8 
-                xl:text-[20px] text-[25px]
-                xl:text-black text-white
-                xl:relative 
-                gradient-text-hover gradient-underline-hover 
-                block 
-                text-center 
-                pt-6 pb-6'
-						>
+					<li className=' xl:border-0 border-2
+									xl:font-thin font-black
+									xl:shadow-none shadow-lg'>
+						<Link   to='/ediciones'
+								className='xl:px-10 xl:py-8 
+								xl:text-[20px] text-[25px]
+								xl:text-black text-white
+								xl:relative 
+								gradient-text-hover gradient-underline-hover 
+								block 
+								text-center 
+								pt-6 pb-6'>
 							Ediciones
 						</Link>
 					</li>
-					<li
-						className=' xl:border-0 border-2
-                        xl:font-thin font-black
-                        xl:shadow-none shadow-lg'
-					>
-						<Link
-							to='/nosotros'
-							className='xl:px-10 xl:py-8 
-                xl:text-[20px] text-[25px]
-                xl:text-black text-white
-                xl:relative 
-                gradient-text-hover gradient-underline-hover 
-                block 
-                text-center 
-                pt-6 pb-6'
-						>
+					<li className=' xl:border-0 border-2
+									xl:font-thin font-black
+									xl:shadow-none shadow-lg'>
+						<Link   to='/nosotros'
+								className='xl:px-10 xl:py-8 
+											xl:text-[20px] text-[25px]
+											xl:text-black text-white
+											xl:relative 
+											gradient-text-hover gradient-underline-hover 
+											block 
+											text-center 
+											pt-6 pb-6'>
 							Nosotros
 						</Link>
 					</li>
