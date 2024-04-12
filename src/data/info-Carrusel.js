447211fe18a653587b3fe776315e0715616d1bba
aszurita <nzuritag@fiec.espol.edu.ec>
2024-04-12 00:00:55 -0500

    Each image with its timer.and NexGen without button

--- src/data/info-Carrusel.js
+++ src/data/info-Carrusel.js
@@ -1,44 +1,26 @@
 export let infoImages = [
 	{
 		id: "1",
+		url: "src/assets/images-carrusel/Copy-of-Welcome030624_20DG.jpg",
+		title: "DATATHON",
+		date: "14 de Junio, 2024",
+		place: "Guayaquil - ESPOL",
+		dateTimer:'June 14, 2024 00:00:00'
+	},
+	{
+		id: "2",
 		url: "src/assets/images-carrusel/imagen1.png",
 		title: "CONFERENCIA",
 		date: "13 de Julio, 2024",
 		place: "Guayaquil - ESPOL",
-	},
-	{
-		id: "2",
-		url: "src/assets/images-carrusel/Copy-of-Welcome030624_20DG.jpg",
-		title: "REUNION",
-		date: "13 de Julio, 2024",
-		place: "Guayaquil - ESPOL",
+		dateTimer:'July 13, 2024 00:00:00'
 	},
 	{
 		id: "3",
 		url: "src/assets/images-carrusel/Carousel-3-Image-Replacement.jpg",
-		title: "CONFERENCIA 2",
+		title: "NEXGEN",
 		date: "13 de Julio, 2024",
 		place: "Guayaquil - ESPOL",
-	},
-	{
-		id: "4",
-		url: "src/assets/images-carrusel/Our-Mission-2-Hero_O-1.jpg",
-		title: "REUNION 2",
-		date: "13 de Julio, 2024",
-		place: "Guayaquil - ESPOL",
-	},
-	{
-		id: "5",
-		url: "src/assets/images-carrusel/Join_Accordion_O.jpg",
-		title: "CONFERENCIA 3",
-		date: "13 de Julio, 2024",
-		place: "Guayaquil - ESPOL",
-	},
-	{
-		id: "6",
-		url: "src/assets/images-carrusel/WiDS_Opportunities.jpg",
-		title: "REUNION3",
-		date: "13 de Julio, 2024",
-		place: "Guayaquil - ESPOL",
+		dateTimer:'July 13, 2024 00:00:00'
 	},
 ];
--- src/pages/Home/Components/Carrusel.jsx
+++ src/pages/Home/Components/Carrusel.jsx
@@ -34,14 +34,14 @@ export function Carrusel() {
                 <BsChevronCompactRight onClick={nextImage} size={30} />
             </div>
             <div className="absolute bottom-[12%] max-lg:bottom-[22%] left-1/2 transform -translate-x-1/2 translate-y-1/2 flex justify-around">
-                <Timer Event_date = {new Date('July 13, 2024 00:00:00')}/>
+                <Timer key={currentIndex} Event_date = {new Date(infoImages[currentIndex].dateTimer)}/>
             </div>
             <div className="absolute top-[10%] left-[5%] text-white select-none font-acumin">
-                <p className="text-4xl font-acumin font-bold">{infoImages[currentIndex].title}</p>
-                <p className="text-lg font-acumin font-normal">{infoImages[currentIndex].date}</p>
-                <p className="text-lg font-acumin font-normal">{infoImages[currentIndex].place}</p>
+                <p className="text-5xl font-acumin font-bold">{infoImages[currentIndex].title}</p>
+                <p className="text-3xl font-acumin font-normal">{infoImages[currentIndex].date}</p>
+                <p className="text-3xl font-acumin font-normal">{infoImages[currentIndex].place}</p>
             </div>
-            <div className="absolute right-[5%] bottom-[9%] max-sm:left-[52%] max-md:left-[57%]  max-lg:left-[60%] max-lg:transform max-lg:-translate-x-1/2 max-lg:translate-y-1/2 ">
+            <div className={`${currentIndex === infoImages.length - 1 ? 'hidden':'block'} absolute right-[5%] bottom-[9%] max-sm:left-[52%] max-md:left-[57%]  max-lg:left-[60%] max-lg:transform max-lg:-translate-x-1/2 max-lg:translate-y-1/2`}>
                 <button className="text-white bg-primary-blue select-none rounded-md px-10 py-3 text-center text-xl font-acumin font-medium">Register</button>
             </div>
 
