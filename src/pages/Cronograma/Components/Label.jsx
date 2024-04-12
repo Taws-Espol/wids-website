ba6a28025e3942b4a73ec4a7ccd9b5ec70a8d7a1
aszurita <nzuritag@fiec.espol.edu.ec>
2024-04-12 01:04:36 -0500

    Responsive checked

--- src/pages/Cronograma/Components/Label.jsx
+++ src/pages/Cronograma/Components/Label.jsx
@@ -1,4 +1,4 @@
-export function Label({type, hiddenDiv}){
+export function Label({type, hiddenDiv,stateHidden}){
     let backgroundColor;
     switch (type) {
         case "Conferencias":
@@ -10,12 +10,18 @@ export function Label({type, hiddenDiv}){
         default:
             backgroundColor = "bg-blue-label";
     }
+    const breakpoint = 1025; 
+    const checkSize = () => {
+        if (window.innerWidth < breakpoint) {
+            hiddenDiv(); 
+        }
+    };
     return(
-        <div class="flex flex-row" onClick={hiddenDiv}>
-            <div class={`${backgroundColor} w-[450px] h-12 text-white font-bold py-2 px-4 flex items-center rounded-l-full overflow-hidden border-r-0 font-acumin max-xsm:w-[284px] max-sm:w-[355px]`}>
-                <p className="text-2xl text-black px-8 max-xsm:px-8 max-xsm:text-xl">{type}</p>
+        <div class="flex flex-row" onClick={checkSize}>
+            <div class={`${backgroundColor} w-[450px] ${stateHidden ? 'h-44  place-content-center':'h-12'}  font-bold p-0 flex items-center mr-0 rounded-l-full overflow-hidden border-r-0 font-acumin max-xsm:w-[284px] max-sm:w-[355px]`}>
+                <p className={`${stateHidden ? 'text-4xl':'text-2xl max-xsm:px-8 max-xsm:text-xl'} text-black px-8`}>{type}</p>
             </div>
-            <div class= {`${backgroundColor} h-12 w-8 triangle max-xsm:w-4 max-sm:w-5`}>
+            <div class= {`${backgroundColor} ${stateHidden ? 'h-44':'h-12'} w-8 triangle mr-0 max-xsm:w-4  border-l-0 max-sm:w-5`}>
             </div>
         </div>
     )
--- src/pages/Cronograma/Components/ListCardInfo.jsx
+++ src/pages/Cronograma/Components/ListCardInfo.jsx
@@ -1,43 +1,52 @@
+import React, { useState, useEffect } from 'react';
 import { Label } from "./Label";
-import { useState } from "react";
 import { CardInfo } from "./CardInfo";
-import {cronograma} from "../../../data/Cronograma.js";
-import { Switch } from "@material-tailwind/react";
+import { cronograma } from "../../../data/Cronograma.js";
 
-export function ListCardInfo({typeLabel}){
-    const [hidden,setHidden] = useState(false);
+export function ListCardInfo({ typeLabel }) {
+    const [hidden, setHidden] = useState(false);
+    const breakpoint = 1025; 
 
-    const changeHiddenValue = () => {
-        setHidden(!hidden);
-    };
+    useEffect(() => {
+        const checkSize = () => {
+            if (window.innerWidth < breakpoint) {
+                setHidden(true); 
+            } else {
+                setHidden(false);
+            }
+        };
+        checkSize();
+        window.addEventListener('resize', checkSize); 
+        return () => {
+            window.removeEventListener('resize', checkSize);
+        };
+    }, []); 
 
-    let listaEventos; 
-    switch(typeLabel){
+    let listaEventos;
+    switch (typeLabel) {
         case "Conferencias":
             listaEventos = cronograma.Conferencias;
             break;
         case "Talleres":
-            listaEventos= cronograma.Talleres;
+            listaEventos = cronograma.Talleres;
             break;
-        default :
-            listaEventos= cronograma.Conferencias;
+        default:
+            listaEventos = cronograma.Conferencias;
             break;
     }
 
-    return(
-        <div className=" flex flex-col gap-15">
-            <Label type={typeLabel} hiddenDiv={changeHiddenValue}/>
-            <div className={`${hidden ? 'hidden':'block'} flex flex-col py-4 gap-4 place-content-center place-items-center`}>
-            {
-                listaEventos.map((evento)=>
+    return (
+        <div className="flex flex-col gap-15">
+            <Label type={typeLabel} hiddenDiv={() => setHidden(!hidden)} stateHidden={hidden}/>
+            <div className={`${hidden ? 'hidden' : 'block'} flex flex-col py-4 gap-4 place-content-center place-items-center`}>
+                {listaEventos.map((evento) =>
                     <CardInfo
                         type={evento.type}
                         title={evento.title}
                         info={evento.info}
                     />
-                )
-            }
+                )}
             </div>
         </div>
-    )
+    );
 }
