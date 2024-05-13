c91877abbc57abfa4d116758a6b75bb40cfa7a28
aszurita <nzuritag@fiec.espol.edu.ec>
2024-05-12 21:45:17 -0500

    Update Card Info

--- src/pages/Edicion/Components/ProfileCard.jsx
+++ src/pages/Edicion/Components/ProfileCard.jsx
@@ -8,43 +8,43 @@ export function ProfileCard({ conferencista }) {
 
   // true hidden
   // false block
-  const [state,setState] = useState(true);
-  const changeState = () =>{
+  const [state, setState] = useState(true);
+  const changeState = () => {
     setState(!state)
   }
   return (
     <div>
-      <Card className={`w-96 h-[450px] ${state ? '':'border-4 border-primary-violet'}`}>
-        <CardHeader floated={false} className="h-80" onClick={changeState}>
-          <img src={image} alt="Conferencista" className="h-80 w-96" />
+      <Card className={`w-96 h-[600px] ${state ? '' : 'border-4 border-primary-violet'}`}>
+        <CardHeader floated={false} className="min-h-96 min-w-96 relative overflow-hidden" onClick={changeState}>
+          <img src={image} alt="Conferencista" className="h-full w-full object-cover" />
         </CardHeader>
-        <CardBody className="text-center">
+        <CardBody className="text-center h-32">
           <p className="text-primary-dark-green font-bold">{conferencista.name}</p>
           <p>{conferencista.work}, {conferencista.place}</p>
         </CardBody>
         <CardFooter className="flex justify-center gap-7 pt-2">
-          { conferencista.linkeding ?
+          {conferencista.linkeding ?
             <Tooltip content="Follow on LinkedIn">
               <a href={conferencista.linkeding} target="_blank" rel="noopener noreferrer">
-                <FaLinkedin size={25} />
+                <FaLinkedin size={30} />
               </a>
             </Tooltip> : <></>
           }
-          { conferencista.x ?
+          {conferencista.x ?
             <Tooltip content="Follow on X">
               <a href={conferencista.x} target="_blank" rel="noopener noreferrer">
-                <FaXTwitter size={25}/>
+                <FaXTwitter size={30} />
               </a>
             </Tooltip> : <></>
           }
         </CardFooter>
       </Card>
-      <Card className={`w-96 max-h-96 overflow-scroll py-10 ${state ? 'hidden':'block border-4 border-primary-violet'}`}>
+      <Card className={`w-96 max-h-96 overflow-scroll py-10 ${state ? 'hidden' : 'block border-4 border-primary-violet'}`}>
         <p className="px-10 text-justify text-2xl">
           {conferencista.info}
         </p>
       </Card>
-    </div>   
+    </div>
 
   )
 }
