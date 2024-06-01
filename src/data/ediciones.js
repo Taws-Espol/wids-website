28821a957b73ff123f7e07f65484c08c97f85e68
aszurita <nzuritag@fiec.espol.edu.ec>
2024-06-01 10:28:06 -0500

    Reviewed merge of branch 'Mariue2', carousel conferences home.

--- src/data/ediciones.js
+++ src/data/ediciones.js
@@ -148,7 +148,7 @@ export const ediciones = [
 				place: "GESIS - Leibniz Institute for the Social Sciences",
 				work: "Asistente de investigación",
 				schedule: "14:20 - 14:40",
-				imageName: "foto1.jpg",
+				imageName: "lisetteespin.png",
 				linkeding:"https://www.linkedin.com/in/lisetteespin",
 				x:"https://twitter.com/lespin",
 				info: "Lisette está cursando su último año de doctorado en la Universidad de Koblenz en Alemania. Trabaja en su tesis doctoral en el departamento Computational Social Science (CSS) en GESIS. Sus intereses se enfocan en la intersección de Network Science y Machine Learning. Actualmente investiga la influencia que tienen las diferentes estructuras de grafos en los algoritmos de clasificación y ranking. Entre el 2017 y 2018, Lisette realizó dos pasantías académicas en la Universidad del Sur de California (USC-ISI) y una en Stanford. Obtuvo su maestría en Ciencias de la Computación en la Universidad de Saarland e hizo una pasantía en el Max Planck Institute for Software Systems (MPI-SWS) en Saarbrücken. Se graduó de ingeniera en Ciencias Computacionales en la ESPOL en el 2010."
@@ -159,7 +159,7 @@ export const ediciones = [
 				place: "DCCU Chile",
 				work: "Profesora asociada",
 				schedule: "14:40 - 15:00",
-				imageName: "foto2.jpg",
+				imageName: "BPoblete.png",
 				language: "español",
 				linkeding:"https://www.linkedin.com/in/bpoblete",
 				x:"https://twitter.com/bpoblete",
@@ -171,7 +171,7 @@ export const ediciones = [
 				place: "Politécnico Rensselaer",
 				work: "Profesora de Ciencias Computacionales",
 				schedule: "15:00 - 15:20",
-				imageName: "foto3.jpg",
+				imageName: "UzmaMushtaque.png",
 				language: "inglés",
 				linkeding:"https://www.linkedin.com/in/uzmamushtaque/",
 				info: "Uzma es profesora en el departamento de Ciencias de la Computación en el Instituto Politécnico Rensselaer, Troy Nueva York. Antes de su trabajo actual, su experiencia laboral más reciente incluye trabajar en EY como Científica de Datos Senior, con su proyecto enfocado en análisis de crédito y riesgo. La investigación de su tesis encuentra una aplicación directa en plataformas en línea de entretenimiento por subscripción y minorista como Amazon y Netflix. Su trabajo de tesis se encuentra en la intersección de cuatro campos: (1) sistemas de recomendación en línea, (2) modelos de elección del consumidor, (3) modelos de cadena de suministro y (4) planificación de surtido. En su investigación actual, desarrolla modelos matemáticos para recomendaciones personalizadas en línea que capturan diferentes efectos de contexto asociados con el comportamiento de selección del usuario individual. Para analizar las herramientas de decisión matemáticas desarrolladas, está usando grandes conjuntos de datos (MovieLens, Amazon Product Review, dataset de venta minorista en línea en el repositorio de UCI, etc). Su tesis incluye una exploración en profundidad, iterativa y metódica de algunos de los grandes datasets disponibles para las calificaciones de preferencia de los consumidores. Logró insights importantes para los sistemas de recomendación en línea a través de un análisis estadístico integral a partir de los resultados generados por sus modelos matemáticos desarrollados, tanto a través de de datos simulados como de los datasets del mundo real mencionados. Su futura investigación incluye la incorporación de mejores representaciones matemáticas del comportamiento del consumidor como resultado del contexto de elección, a través de enfoques basados en datos no paramétricos y el uso de analítica de datos para mejorar los modelos de elección del consumidor. Su formación en ingeniería, experiencia laboral en consultoría ERP, una maestría en cadena de suministros y su investigación actual motiva la naturaleza interdisciplinaria de sus intereses de investigación."
@@ -182,7 +182,7 @@ export const ediciones = [
 				place: "Banco del Pacífico",
 				work: "Arquitecta de datos",
 				schedule: "15:25 - 15:45",
-				imageName: "foto4.jpg",
+				imageName: "estefanialozano.png",
 				linkeding:"https://www.linkedin.com/in/estefania-lozano-jara-95520b94/",
 				x:"https://twitter.com/onazol11",
 				info: "Ingeniera en Ciencias Computacionales graduada de la ESPOL, promotora del software libre y evangelizadora de la enseñanza de programación a niños y niñas. Actualmente, trabaja en el Banco del Pacífico como arquitecta de datos y líder técnico en la implementación de un datalake con gobernanza de datos. La misión del proyecto es evangelizar a todas las áreas del banco para que usen la plataforma en sus casos de uso que requieran Big data. Fue seleccionada como embajadora de innovación y vocera de transformación cultural del banco."
@@ -193,7 +193,7 @@ export const ediciones = [
 				place: "ESPOL",
 				work: "Profesora e investigadora",
 				schedule: "15:45 - 16:05",
-				imageName: "foto5.jpg",
+				imageName: "VanessaEcheverria.png",
 				linkeding:"https://www.linkedin.com/in/vanessaecheverria/",
 				x: "https://twitter.com/vanechev",
 				info: "PhD. en Análisis de Aprendizaje en el Centro de Inteligencia Conectada, Universidad de Tecnología, Sydney. Particularmente, sus intereses de investigación incluyen análisis de aprendizaje multimodal, aprendizaje automático, inteligencia artificial y diseño centrado en el ser humano en espacios presenciales. Actualmente es profesora en la Facultad de Ingeniería Eléctrica y Computación de la Escuela Superior Politécnica del Litoral (ESPOL) en Guayaquil, Ecuador. En trabajos anteriores, trabajó como Investigadora en el Centro de Tecnologías de la Información (CTI), con la Prof. Katherine Chiluiza. Durante este tiempo, exploró diferentes tecnologías para apoyar el desarrollo de habilidades de trabajo en equipo a través de un sistema colaborativo mejorado por la tecnología y para proporcionar retroalimentación automatizada de habilidades de presentación oral."
@@ -204,7 +204,7 @@ export const ediciones = [
 				place: "OPEV",
 				work: "Directora Ejecutiva",
 				schedule: "16:05 - 16:25",
-				imageName: "foto6.jpg",
+				imageName: "anamariapesante.png",
 				linkeding:"https://www.linkedin.com/in/anamariapesantesconsultant/",
 				info: "Máster en Dirección de Empresas del IDE Business School, Máster en Marketing y Gestión Comercial de la Universidad de Barcelona (UB) y EAE Business School. También realizó diferentes programas como; Diplomado de Igualdad y Empoderamiento de la Mujer de la Universidad de Salamanca, Certificada por la ONU en Igualdad de Género, Programa de Desarrollo Directivo (PADE), Gestión en Proyectos, Marketing Relacional y Digital, Estrategia de Comunicación y Formación de Mujeres en el IDE Business School, además de un Diplomado en Marketing y Ventas en la Universidad de Especialidades Empresarial (UEES). Es ingeniera en Administración y Marketing Estratégico de la Universidad Casa Grande de Guayaquil. Con 40 años, es Directora Ejecutiva de OPEV® – Organización y Promoción para la Economía Violeta, una ONG sin fines de lucro, que tiene como base la investigación aplicada que proporciona la evidencia científica, recomendaciones innovadoras y soluciones concretas en diferentes escalas del ciclo de vida de la mujer para plantear estrategias en los ODS 5, 1, 4, 8,9 y 10. Co-Autora de la teoría y propiedad intelectual de - Economía Violeta® – que se define como una estrategia cuyo objetivo buscar reducir la brecha de género estableciendo las “mismas reglas del juego” para hombres y mujeres en el ámbito social, educativo, económico y político a través de la igualdad, competitividad y sostenibilidad. Adicionalmente, es presidenta de la primera y única agencia consultora de Marketing e Innovación con perspectiva en género llamada Bridge The Gap® Ecuador, una franquicia con matriz en Argentina especializada en procesos de transformación cultural en Diversidad, Equidad de Género, Sustentabilidad, Comunicación y Bienestar Organizacional. Además, es representante de los derechos de comercialización para Ecuador de Blakcs Worth of Knowledge una de las empresas más emblemáticas de marketing y negocios en el mundo, ya que es un HUB integrado por marcas como; WOBI, CDU, Re-think, Universidad de Chicago, Marketing Tech y Apprecia. También es Miembro del Directorio de la Asociación de Marketing del Ecuador, Miembro del Directorio de la Asociación de Jóvenes Emprendedores del Ecuador, conferencista del Women Economic Forum 2019 y Miembro del Jurado del Festival AMAPRO 2019 en México. Se desempeñó en el Banco Pichincha donde hizo una carrera de 9 años, fue Directora Junior de la Cámara de Comercio de Guayaquil y Gerente de Marketing en Las Fragancias."
 			},
@@ -214,7 +214,7 @@ export const ediciones = [
 				place: "Universidad de Stanford",
 				work: "Ph.D. Student",
 				schedule: "16:30 - 16:50",
-				imageName: "foto7.jpg",
+				imageName: "CindyOrozco.png",
 				linkeding:"https://www.linkedin.com/in/cindycorozco/",
 				info: "Cindy es una candidata a doctorado en Ingeniería Computacional y Matemática en Stanford. Tiene una maestría en Matemáticas Aplicadas de KAUST, Arabia Saudita e hizo su pregado en Ingeniería Civil y Matemáticas en la Universidad de los Andes, Colombia. Siempre se ha interesado en descubrir y dar a conocer el valor de las matemáticas en las aplicaciones cotidianas, y eso la ha llevado a trabajar en simulación computacional y ahora en ciencia de datos. En su tiempo libre, pinta y camina en la naturaleza."
 			},
@@ -224,7 +224,7 @@ export const ediciones = [
 				place: "ESPOL",
 				work: "Decana de grado de ESPOL",
 				schedule: "16:50 - 17:10",
-				imageName: "foto8.jpg",
+				imageName: "aliciaguerrero.png",
 				x:"https://twitter.com/alicrisguerrero",
 				info: "Alicia Guerrero, decana de grado de ESPOL y profesora titular de las materias de: Economía de los Mercados y Regulación, Introducción a la Microeconomía, Microeconomía I, II y III, Desarrollo Social y Voluntariado. Además consta con una Maestría en Economía, Universidad Alberto Hurtado de Chile. Master of Arts in Economics, ILADES-Georgetown University. Además es miembro del equipo del Ecuador participante del proceso de certificación de Oro de Comercio Justo. Proyecto auspiciado por la Asociación por la Minería Responsable (Colombia) y FLO – CERT."
 			},
@@ -234,7 +234,7 @@ export const ediciones = [
 				place: "ESPOL",
 				work: "Profesora de la Facultad de Ciencias Naturales y Matemáticas",
 				schedule: "17:10 - 17:30",
-				imageName: "foto9.jpg",
+				imageName: "heydiroa.png",
 				linkeding:"https://www.linkedin.com/in/heydi-roa-4325594a/",
 				x: "https://twitter.com/heydiroa",
 				info: "Ingeniera en Estadística Informática. Magíster en Control de Operaciones y Logística. Consultora con 10 años de experiencia en Logística y estudios estadísticos. Ha desarrollado proyectos de reingeniería, control de procesos y optimización en la distribución de pequeñas y medianas empresas. Ha realizado estudios de intención de votos en dos elecciones a alcalde de un cantón de la Península de Santa Elena. Actualmente se encuentra formando parte de un proyecto de Resiliencia frente a inundaciones de la ciudad de Guayaquil donde participa en la parte estadística desarrollando el indicador de vulnerabilidad frente a inundaciones de la población de la parroquia Febres Cordero."
@@ -245,7 +245,7 @@ export const ediciones = [
 				place: "Contífico",
 				work: "Gerente de Operaciones",
 				schedule: "17:35 - 17:55",
-				imageName: "foto10.jpg",
+				imageName: "estefaniamoran.png",
 				linkeding:"https://www.linkedin.com/in/estefan%C3%ADa-mor%C3%A1n-23879230/",
 				x:"https://twitter.com/EstefyMoran",
 				info: "Ingeniera en Ciencias Computacionales, le interesa la rama de Big Data, es Cofundadora y Gerente de Operaciones de Contífico, una plataforma en línea para la gestión contable y financiera de las pymes. En esta startup se encarga del área de tecnología en donde mantiene y desarrolla el software y las nuevas herramientas, así como también realiza el mantenimiento de la infraestructura de servidores y datos."
@@ -256,7 +256,7 @@ export const ediciones = [
 				place: "Universidad Casa Grande",
 				work: "Profesora en la Facultad de Comunicación",
 				schedule: "17:55 - 18:15",
-				imageName: "foto11.jpg",
+				imageName: "gabrielabaquerizo.png",
 				linkeding:"https://www.linkedin.com/company/widsespol/",
 				x:"https://twitter.com/widsespol",
 				info: "PhD. por la Universidad Austral de Buenos Aires, Argentina y magíster en DIRCOM y Nuevas Tecnologías de la Información, España. Docente Investigadora en la Universidad de Casa Grande en áreas como: reputación, liderazgo político femenino, comunicación estratégica, redes sociales y sentiment analysis. Miembro del grupo de Gestión de la Comunicación Estratégica, miembro de la Red de Investigadores en Comunicación de Ecuador (RICE) y colaboradora de la Red Internacional de Investigación de la Gestión de la Comunicación (XESCOM). Coordinadora y autora del libro 'La comunicación política en campaña. Identidad, imagen y lenguaje como elementos estratégicos en campañas políticas latinoamericanas'. Publicaciones en Journal of Social Informatics, IEEE Transactions on Emerging Topics in Computing, Revista de Comunicación, revista Trípodos, Revista Ibérica de Sistemas y Tecnologías de Información (RISTI), entre otras."
@@ -267,7 +267,7 @@ export const ediciones = [
 				place: "ESPOL",
 				work: "Directora de Investigación de grado de FIEC",
 				schedule: "18:15 - 18:35",
-				imageName: "foto12.jpg",
+				imageName: "cristinaabad.png",
 				linkeding:"https://www.linkedin.com/in/gabrielabaquerizo/",
 				x:"https://twitter.com/gabybneira",
 				info: "Cristina Abad es profesora de la ESPOL, dirige el Laboratorio de Investigación de Sistemas Distribuidos y co-dirige el Grupo de Investigación de Big Data. Recibió su Ph.D. de la Universidad de Illinois en Urbana-Champaign. Del 2011-2014 fue parte del equipo de desarrollo de Hadoop en Yahoo, donde buscó mejorar el rendimiento del HDFS. Sus intereses yacen en la intersección de Sistemas Distribuidos e Ingeniería de Rendimiento en sistemas de la infraestructura de Big Data y Cloud Computing. Ha recibido financiamiento de Vlir-UOS, Google, Microsoft, Amazon Web Services y AT&T Labs Research. Ha recibido una beca Fulbright, el UIUC CS Excellence Fellowship y dos Google Faculty Research Awards."
