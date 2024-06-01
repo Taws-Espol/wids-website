ef09a37d07a3ba96ecd5b517cb3f6676cee67bb7
nahinespinoza <nahiespi@espol.edu.ec>
2024-06-01 16:48:29 -0500

    Resolviendo conflicto despues del pull desde main

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -31,9 +31,15 @@ function Footer() {
 
     return (
       <footer className='font-acumin'>
-        <ul className={`bg-[rgb(0,0,0)] grid xl:grid-cols-2 grid-cols-1 w-full px-4 py-4 ${nothome ? 'hidden':'block'}`}>
+        <ul className={`bg-[rgb(231,231,231)] grid xl:grid-cols-2 grid-cols-1 w-full px-4 py-4 ${nothome ? 'hidden':'block'}`}>
           <li className="px-20 pt-10 xl:text-start text-center">
-            <h2 className="text-[35px] font-semibold text-white mb-10">Contacto</h2>
+            <h2 className="text-[35px] font-sans font-semibold text-black mb-3">Contacto</h2>
+            <h3 className="text-lg font-sans text-gray-500 mb-12">wids@fiec.espol.edu.ec</h3> 
+
+            <img src="/src/assets/wids-logo.png" className="mx-auto"/>
+
+            
+
           </li>
           <li className="px-20 text-center">
             <br className='xl:block hidden'/>
@@ -50,7 +56,7 @@ function Footer() {
                 <input
                   type="text"
                   name="user_name"
-                  className="bg-transparent border-b-2 border-white w-full text-white focus:outline-none py-1"
+                  className="bg-transparent border-b-2 border-[rgb(0,167,213)] w-full font-sans text-black focus:outline-none py-1"
                   placeholder="Nombre"
                 />
               </div>
@@ -62,8 +68,8 @@ function Footer() {
                 <input
                   type="email"
                   name="user_email"
-                  className="bg-transparent border-b-2 border-white w-full text-white focus:outline-none py-1"
-                  placeholder="Correo electrónico"
+                  className="bg-transparent border-b-2 border-[rgb(74,185,105)] w-full font-sans text-black focus:outline-none py-1"
+                  placeholder="Correo Electrónico"
                 />
               </div>
               <div className="mb-6">
@@ -74,8 +80,8 @@ function Footer() {
                 <input
                   type="text"
                   name="Subject"
-                  className="bg-transparent border-b-2 border-white w-full text-white focus:outline-none py-1"
-                  placeholder="Asunto del mensaje"
+                  className="bg-transparent border-b-2 border-[rgb(255,203,5)] w-full font-sans bg-opacity-5 text-black focus:outline-none py-1 "
+                  placeholder="Asunto"
                 />
               </div>
               <div className="mb-10">
@@ -86,12 +92,12 @@ function Footer() {
                 <textarea
                   name="message"
                   rows="4"
-                  className="bg-transparent border-2 border-white w-full text-white focus:outline-none py-1 px-2"
-                  placeholder="mensaje"
+                  className="bg-transparent border-2 border-black w-full font-sans text-black focus:outline-none py-1 px-2 rounded-md"
+                  placeholder="Mensaje"
                 ></textarea>
               </div>
-              <div className="mb-10 xl:text-end">
-                <input type="submit" value="Enviar" className="cursor-pointer border-1 bg-white px-3 py-2 rounded-md"/>
+              <div className="mb-10 flex justify-center">
+                <input type="submit" value="Enviar" className="cursor-pointer  border-1 bg-[rgb(0,158,202)] font-sans font-semibold text-xl text-white px-5 py-1 rounded-lg"/>
               </div>
             </form>
           </li>
