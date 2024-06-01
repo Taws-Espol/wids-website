021666347fe975b5bfb084f68112e40ff1c4c487
aszurita <nzuritag@fiec.espol.edu.ec>
2024-06-01 10:20:04 -0500

    Reviewed merge of branch 'next gen'

--- src/data/sponsors.js
+++ src/data/sponsors.js
@@ -3,41 +3,41 @@ export const sponsors = [
         "name": "Sherpa.ai",
         "location": "B14",
         "website": "https://sherpa.ai/",
-        "logo": "/public/assets/sherpa.png"
+        "logo": "/assets/sherpa.png"
     },
     {
         "name": "Microsoft",
         "location": "A10",
-        "logo": "/public/assets/microsoft.png"
+        "logo": "/assets/microsoft.png"
     },
     {
         "name": "Iris-AI",
         "location": "B07",
-        "logo": "/public/assets/iris.png"
+        "logo": "/assets/iris.png"
     },
     {
         "name": "Tamr",
         "location": "B11",
-        "logo": "/public/assets/tamr.png"
+        "logo": "/assets/tamr.png"
     },
     {
         "name": "Next IT",
         "location": "B06",
-        "logo": "/public/assets/next.png"
+        "logo": "/assets/next.png"
     },
     {
         "name": "Google",
         "location": "A01",
-        "logo": "/public/assets/google.png"
+        "logo": "/assets/google.png"
     },
     {
         "name": "Cognitive Scale",
         "location": "A05",
-        "logo": "/public/assets/cognitive.png"
+        "logo": "/assets/cognitive.png"
     },
     {
         "name": "AlphaSense",
         "location": "B15",
-        "logo": "/public/assets/alpha.png"
+        "logo": "/assets/alpha.png"
     }
 ]
--- src/pages/Eventos/NextGen.jsx
+++ src/pages/Eventos/NextGen.jsx
@@ -8,14 +8,14 @@ function NextGen() {
     const { title, requirements } = NextGenInfo[2];
     const { alt, link_image, info } = NextGenInfo[3];
     return (
-        <div className="w-full flex flex-col place-content-center place-items-center pb-10 font-acumin ">
+        <div className="w-full flex flex-col place-content-center place-items-center pb-10 font-acumin select-none">
             <div className="w-full px-4 sm:px-8 md:px-16 lg:px-32 xl:px-64">
                 <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl font-bold font-acumin text-primary-dark-green text-center  lg:text-left py-0 sm:py-5">
                     Next Gen 2024
                 </h1>
             </div>
             <div className="w-4/5 h-auto rounded-lg pb-0  flex flex-col place-content-center place-items-center">
-                <div id='div_nextgen-empoderando' className="flex max-lg:flex-col max-sm:min-w-[300px] place-content-center place-items-center mb-10">
+                <div id='div_nextgen-empoderando' className="flex max-lg:flex-col max-sm:min-w-[300px] place-content-center place-items-center mb-10 max-sm:gap-10">
                     <div className="w-3/5 flex justify-center items-center text-center relative mt-10">
                         <div id='rectangulo_blue' className="absolute top-[-30px] right-10 w-56 h-56 bg-custom-peach z-10 max-lg:hidden"></div>
                         <div id='circulo-yellow' className="absolute bottom-[-52px] left-5 h-52 w-52 rounded-full bg-custom-purple z-10 max-lg:hidden"></div>
@@ -31,7 +31,7 @@ function NextGen() {
                     </div>
                 </div>
                 <div className="flex flex-col lg:flex-row m-8 w-full bg-custom-white rounded-md  py-10">
-                    <div className="w-full lg:w-1/2 ml-0 flex items-center justify-center lg:pl-6 ml-0" >
+                    <div className="w-full lg:w-1/2 flex items-center justify-center lg:pl-6 ml-0" >
                         <h1 className="text-center text-primary-dark-green text-5xl lg:text-6xl font-bold py-5 ">{NextGenInfo[1].title}</h1>
                     </div>
                     <div className="w-full lg:w-2/3 mt-8 lg:mt-0">
@@ -62,10 +62,7 @@ function NextGen() {
                 <div id='div_nextgen-empoderando' className="flex max-lg:flex-col max-sm:min-w-[300px] place-content-center place-items-center">
                     <div id='nextgen-empoderando' className=" ml-10 text-2xl w-2/5 text-justify px-4 flex flex-col gap-5 max-sm:text-base  max-md:text-lg  max-lg:text-xl  max-lg:w-full max-lg:m-0">
                         <p>
-                            {text1[0]}<b>{text1[1]}</b>
-                        </p>
-                        <p>
-                            {div1.info[1]}
+                            {info}
                         </p>
                     </div>
                     <div className="w-3/5 flex justify-center items-center text-center relative  mt-10">
@@ -73,7 +70,7 @@ function NextGen() {
                         </div>
                         <div id='circulo-yellow' className="absolute bottom-[-52px] right-5 h-52 w-52 rounded-full bg-custom-purple z-10 max-lg:hidden">
                         </div>
-                        <img src={div1.link_image} alt="NextGen-Empoderando a la siguiente generación" className="w-4/5 h-[350px] z-20 max-sm:min-w-[300px] max-sm:h-[200px] max-md:min-w-[550px] max-md:h-[300px]  max-lg:min-w-[600px] max-lg:h-[400px] max-lg:rounded-2xl" />
+                        <img src={link_image} alt="NextGen-Empoderando a la siguiente generación" className="w-4/5 h-[350px] z-20 max-sm:min-w-[300px] max-sm:h-[200px] max-md:min-w-[550px] max-md:h-[300px]  max-lg:min-w-[600px] max-lg:h-[400px] max-lg:rounded-2xl" />
                     </div>
                 </div>
             </div>
