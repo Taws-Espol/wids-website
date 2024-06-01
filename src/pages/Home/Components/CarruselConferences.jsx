77cabbc8eb57877ec04b0c1a0164b7652abe0db7
MariuAndrade <mrandrad@fiec.espol.edu.ec>
2024-06-01 09:50:58 -0500

    carrusel
    cambios carrusel

--- src/pages/Home/Components/CarruselConferences.jsx
+++ src/pages/Home/Components/CarruselConferences.jsx
@@ -1,65 +1,74 @@
 import { ediciones } from '../../../data/ediciones';
-
 import React from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-
-
+import { FaArrowLeft, FaArrowRight } from 'react-icons/fa'; // Importa los iconos de flecha
 
 export default function CarruselConferences() {
 
     const edicion2020 = ediciones.find(edicion => edicion.edicion === "2020");
 
+    const CustomPrevArrow = (props) => (
+        <div {...props}>
+            <FaArrowLeft color="black" size={20} />
+        </div>
+    );
+
+    const CustomNextArrow = (props) => (
+        <div {...props}>
+            <FaArrowRight color="black" size={20} />
+        </div>
+    );
+
     var settings = {
         dots: true,
         infinite: true,
-        slidesToShow: 5,
+        slidesToShow: 3,
         slidesToScroll: 1,
         autoplay: true,
         autoplaySpeed: 2000,
-        pauseOnHover: true, 
+        pauseOnHover: false,
+        nextArrow: <CustomNextArrow />, // Usa las flechas personalizadas
+        prevArrow: <CustomPrevArrow />, // Usa las flechas personalizadas
         responsive: [
             {
-              breakpoint: 1024,
-              settings: {
-                slidesToShow: 3,
-                slidesToScroll: 1,
-                infinite: true,
-                dots: true
-              }
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 1,
+                    infinite: true,
+                    dots: true
+                }
             },
             {
-              breakpoint: 600,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1,
-                initialSlide: 2
-              }
+                breakpoint: 600,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    initialSlide: 2
+                }
             },
             {
-              breakpoint: 480,
-              settings: {
-                slidesToShow: 1,
-                slidesToScroll: 1
-              }
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1
+                }
             }
-          ]
+        ]
     };
-
-
     return (
-        
-        <div className="relative h-[500px]" >
+        <div className="relative h-[500px]">
             <div className="absolute inset-0">
-                <div className="h-2/6 bg-primary-dark-green"></div>
-                <div className="h-3/6 bg-white"></div>
-                <div className="h-1/6 bg-primary-dark-green"></div>
+                <div className="h-[200px] bg-primary-dark-green"></div>
+                <div className="h-[200px] bg-white"></div>
+                <div className="h-[100px] bg-primary-dark-green"></div>
             </div>
 
             <div className="relative w-3/4 m-auto ">
 
-                <h1 className="pt-6 md:pt-10 text-center text-[6vw] md:text-[4vw] 2xl:text-[3vw] min-[2474px]:text-[2.25vw] font-serif font-bold" style={{color:"white"}}>
+                <h1 className="pt-6 md:pt-10 text-center text-[6vw] md:text-[4vw] 2xl:text-[3vw] min-[2474px]:text-[2.25vw] font-serif font-bold text-white">
                     Conferencistas
                 </h1>
                 <Slider {...settings}>
@@ -69,7 +78,7 @@ export default function CarruselConferences() {
                                 <img src={'src/assets/pruebaMaru/' + evento.imageName} alt={evento.title} className="h-44 w-44 rounded-full" />
                             </div>
 
-                            <div className="flex flex-col justify-center items-center pt-2 pb-8">
+                            <div className="flex flex-col justify-center items-center pt-2 pb-12">
                                 <p style={{ fontSize: '16px', fontWeight: 'bold', color: 'black' }}> {evento.name}</p>
                                 <p style={{ fontSize: '14px', color: 'grey' }}>{evento.name}</p>
                             </div>
@@ -79,9 +88,8 @@ export default function CarruselConferences() {
                 </Slider>
             </div>
         </div>
-
-
     );
 }
+
 
 //  https://react-slick.neostack.com/docs/example/resizable 
