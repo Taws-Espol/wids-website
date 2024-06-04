e29e8de2de1fb1c277c40740a94193f56dde3ade
aszurita <nzuritag@fiec.espol.edu.ec>
2024-06-03 22:07:54 -0500

    Fix carousel

--- src/pages/Home/Components/Infos.jsx
+++ src/pages/Home/Components/Infos.jsx
@@ -3,8 +3,8 @@ export function Infos() {
     return (
         <div className="relative flex flex-row max-xl:flex-wrap place-content-center place-items-center p-5 md:w-[1000px] font-acumin gap-0 xl:pb-48 max-xl:gap-10 select-none">
                 <Contador number={5} text={"     Edición"} color="text-primary-green" className="xl:absolute xl:top-[5px] xl:left-[80px]" position={"ta"}/>
-                <Contador number={12} text={"Conferencistas"} color="text-primary-blue" className="xl:absolute xl:top-[50px] xl:left-[300px]"/>
-                <Contador number={"+20"} text={"Áreas Involucradas"} color="text-primary-orange" className="xl:absolute xl:top-[100px] xl:left-[550px]"/>
+                <Contador number={12} text={"Conferencistas"} color="text-primary-blue" className="xl:absolute xl:top-[50px] xl:left-[280px]"/>
+                <Contador number={"+20"} text={"Áreas Involucradas"} color="text-primary-orange" className="xl:absolute xl:top-[100px] xl:left-[530px]"/>
                 <Contador number={2200} text={"Visualizaciones en Youtube"} color="text-primary-dark-green" className="xl:absolute xl:top-[150px] xl:left-[850px]"/>
         </div>
     );
