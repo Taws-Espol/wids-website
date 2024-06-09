1a9a8768a5ba73c1f5900ab02e03d4513f6ca52d
aszurita <nzuritag@fiec.espol.edu.ec>
2024-06-09 15:20:55 -0500

    Animation

--- src/pages/Home/Components/Sponsors.jsx
+++ src/pages/Home/Components/Sponsors.jsx
@@ -1,19 +1,28 @@
 import { sponsors } from "../../../data/sponsors";
 import Sponsor from "./Sponsor";
 import Subtitle from "./Subtitle";
+import AnimatedSection from "./../../../shared/AnimatedSection";
 export default function Sponsors() {
 	return (
 		<div className={`flex flex-col items-center p-20 select-none`}>
-			<Subtitle text='Organizador' />
-			<div className='my-20'>
-				<img src='/assets/taws.png' alt='Taws' width={200} className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"/>
-			</div>
-			<Subtitle text='Sponsors' />
-			<div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 gap-y-12 gap-x-10'>
-				{sponsors.map((sponsor, index) => (
-					<Sponsor key={index} {...sponsor} />
-				))}
-			</div>
+			<AnimatedSection>
+				<Subtitle text='Organizador' />
+			</AnimatedSection>
+			<AnimatedSection>
+				<div className='my-20'>
+					<img src='/assets/taws.png' alt='Taws' width={200} className="transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300"/>
+				</div>
+			</AnimatedSection>
+			<AnimatedSection>
+				<Subtitle text='Sponsors' />
+			</AnimatedSection>
+			<AnimatedSection>
+				<div className='grid sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3 my-10 gap-y-12 gap-x-10'>
+					{sponsors.map((sponsor, index) => (
+						<Sponsor key={index} {...sponsor} />
+					))}
+				</div>
+			</AnimatedSection>
 		</div>
 	);
 }
--- src/pages/Nosotros/Nosotros.jsx
+++ src/pages/Nosotros/Nosotros.jsx
@@ -1,41 +1,46 @@
 import  AboutUsCard  from "./Components/AboutUsCard";
+import AnimatedSection from "./../../shared/AnimatedSection";
 function Nosotros() {
   return (
     <>
-      <div
-        className="flex px-5 justify-center 
-          md:justify-start py-7 sm:pl-20 md:pl-40"
-      >
-        <h1
-          className="text-sans text-[#1e4620] text-center md:text-left
-            font-bold text-7xl sm:text-5xl md:text-5xl lg:text-6xl select-none"
+      <AnimatedSection>
+        <div
+          className="flex px-5 justify-center 
+            md:justify-start py-7 sm:pl-20 md:pl-40"
         >
-          Sobre Nosotros
-        </h1>
-      </div>
-      <section className="w-full select-none pt-5 pb-20">
-        <div className="flex flex-col md:flex-row gap-5">
-          <div
-            className=" flex-grow md:w-1/2 px-5 grid
-            place-content-center place-items-center"
+          <h1
+            className="text-sans text-[#1e4620] text-center md:text-left
+              font-bold text-7xl sm:text-5xl md:text-5xl lg:text-6xl select-none"
           >
-            <AboutUsCard/>
-          </div>
-          <div className="flex-grow md:w-1/2 pl-5 pr-10 grid place-content-center place-items-center">
-            <p className="!text-justify md:text-left text-xs sm:text-base md:text-lg lg:text-xl xl:text-xl font-acumin">
-              {/* Texto de Lorem Ipsum */}
-              WiDS Guayaquil@ESPOL es un evento independiente organizado por la
-              PhD. Carmen Vaca con sede en la Escuela Superior Politécnica del
-              Litoral (ESPOL), que coincide con la conferencia anual global de
-              Women in Data Science (WiDS) realizada por la Universidad de
-              Stanford y aproximadamente más de 150 localidades en todo el
-              mundo. Todas las personas están invitadas a asistir a los eventos
-              regionales de WiDS, que presentan mujeres destacadas que realizan
-              un trabajo excepcional.
-            </p>
-          </div>
+            Sobre Nosotros
+          </h1>
         </div>
-      </section>
+      </AnimatedSection>
+      <AnimatedSection>
+        <section className="w-full select-none pt-5 pb-20">
+          <div className="flex flex-col md:flex-row gap-5">
+            <div
+              className=" flex-grow md:w-1/2 px-5 grid
+              place-content-center place-items-center"
+            >
+              <AboutUsCard/>
+            </div>
+            <div className="flex-grow md:w-1/2 pl-5 pr-10 grid place-content-center place-items-center">
+              <p className="!text-justify md:text-left text-xs sm:text-base md:text-lg lg:text-xl xl:text-xl font-acumin">
+                {/* Texto de Lorem Ipsum */}
+                WiDS Guayaquil@ESPOL es un evento independiente organizado por la
+                PhD. Carmen Vaca con sede en la Escuela Superior Politécnica del
+                Litoral (ESPOL), que coincide con la conferencia anual global de
+                Women in Data Science (WiDS) realizada por la Universidad de
+                Stanford y aproximadamente más de 150 localidades en todo el
+                mundo. Todas las personas están invitadas a asistir a los eventos
+                regionales de WiDS, que presentan mujeres destacadas que realizan
+                un trabajo excepcional.
+              </p>
+            </div>
+          </div>
+        </section>
+      </AnimatedSection>
     </>
   );
 }
