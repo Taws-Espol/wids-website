040fd07b6cd79a4882db749dcc3cd1070f71dd16
aszurita <nzuritag@fiec.espol.edu.ec>
2024-06-10 11:27:24 -0500

    Cambio a webp

--- src/pages/Conferencistas/Conferencistas.jsx
+++ src/pages/Conferencistas/Conferencistas.jsx
@@ -9,7 +9,7 @@ function Conferencistas() {
                 <div id='rectangulo_corneL' className="absolute bottom-[-30px] left-9 max-xl:left-4 w-1/4 h-48 bg-custom-peach max-lg:hidden z-[5]"></div>
                 <div id='rectangulo_corneR' className="absolute top-[-30px] right-9 max-xl:right-4 w-1/4 h-48 bg-custom-purple max-lg:hidden z-[5]"></div>
                 <div className=" flex flex-col w-full items-center place-content-center place-items-center max-lg:mt-32">
-                    <img src="/assets/Conferences/conference.png" alt="conference" className="w-11/12 h-96 object-cover object-top relative z-[10]" />
+                    <img src="/assets/Conferences/conference.webp" alt="conference" className="w-11/12 h-96 object-cover object-top relative z-[10]" />
                     </div>
                 <div id='rectangulo_text' className=" absolute top-[-30px] lg:left-20 lg:w-96 h-34 left-0 w-full bg-custom-gre z-[15] ">
                     <h1 className="text-5xl font-bold mb-4 mt-2 text-white">Conferencistas</h1>
