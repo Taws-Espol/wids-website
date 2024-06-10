df4715f8cb441e04442c82a4a70dcb564fe611e2
aszurita <nzuritag@fiec.espol.edu.ec>
2024-06-09 21:58:22 -0500

    Ubication map react

--- src/pages/Home/Components/Ubicacion.jsx
+++ src/pages/Home/Components/Ubicacion.jsx
@@ -1,34 +1,82 @@
-import { MdLocationPin } from "react-icons/md";
-const Ubicacion = () => {
-    return (
-        <div className="bg-neutral-grey bg-opacity-20 text-primary-dark-green">
-            <div className="container mx-auto py-6 px-8">
-                <div className="text-center">
-                    <h1 className="text-4xl font-bold" >Ubicación</h1>
-                </div>
-            </div>
+import React, { useEffect, useState } from 'react';
+import { APIProvider, Map, useMapsLibrary, useMap } from '@vis.gl/react-google-maps';
 
-            <div className="container mx-auto flex flex-col md:flex-row items-center md:items-start px-4 md:px-20 pb-20 pt-8 select-none">
-                <div className="md:w-1/2"> 
-                    <iframe
-                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1494.5011907822418!2d-79.96631895396388!3d-2.145020582613316!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x902d72f925e5bfdb%3A0x327cdb9f7f4ba3b2!2sEscuela%20Superior%20Polit%C3%A9cnica%20del%20Litoral%20(ESPOL)!5e0!3m2!1ses-419!2sec!4v1712382387776!5m2!1ses-419!2sec" 
-                        width="650"
-                        height="900"
-                        style={{ border: 0 }}
-                        allowFullScreen=""
-                        loading="lazy"
-                        referrerPolicy="no-referrer-when-downgrade"
-                        className="w-45 h-96 max-w-full shadow-md rounded-lg" 
-                    ></iframe>
+const Ubicacion = () => (
+    <APIProvider apiKey={"AIzaSyCnVttffZ4MLtTnpnFTWChCzUZMv16dJq4"}>
+        <Map
+            defaultZoom={15}
+            gestureHandling={'greedy'}
+            fullscreenControl={true}
+            style={{ height: '500px', width: 'w-full' }}>
+            <Directions origin={{ lat: -2.143737, lng: -79.967578 }} destination={{ lat: -2.143496, lng: -79.966081 }} />
+        </Map>
+    </APIProvider>
+);
+
+function Directions({ origin, destination }) {
+    const map = useMap();
+    const routesLibrary = useMapsLibrary('routes');
+    const [directionsService, setDirectionsService] = useState(null);
+    const [directionsRenderer, setDirectionsRenderer] = useState(null);
+    const [routes, setRoutes] = useState([]);
+    const [routeIndex, setRouteIndex] = useState(0);
+    const selected = routes[routeIndex];
+    const leg = selected?.legs[0];
+
+    useEffect(() => {
+        if (!routesLibrary || !map) return;
+        setDirectionsService(new routesLibrary.DirectionsService());
+        setDirectionsRenderer(new routesLibrary.DirectionsRenderer({ map }));
+    }, [routesLibrary, map]);
+
+    useEffect(() => {
+        if (!directionsService || !directionsRenderer) return;
+
+        directionsService
+            .route({
+                origin: origin,
+                destination: destination,
+                travelMode: google.maps.TravelMode.DRIVING,
+                provideRouteAlternatives: true
+            })
+            .then(response => {
+                directionsRenderer.setDirections(response);
+                setRoutes(response.routes);
+            });
+
+        return () => directionsRenderer.setMap(null);
+    }, [directionsService, directionsRenderer, origin, destination]);
+
+    useEffect(() => {
+        if (!directionsRenderer) return;
+        directionsRenderer.setRouteIndex(routeIndex);
+    }, [routeIndex, directionsRenderer]);
+
+    if (!leg) return null;
+
+    return (
+        <div className="absolute top-0 left-0 !max-w-96 !max-h-96 !overflow-hidden flex flex-col">
+            <div className="bg-primary-dark-green  text-white">
+                <div className="container mx-auto py-6 px-8">
+                    <div className="text-center">
+                        <h1 className="text-4xl font-bold">Ubicación</h1>
+                    </div>
                 </div>
-                <div className="md:w-1/2 mt-4 md:mt-0 md:ml-8">
-                    <div className='flex flex-col place-content-start  px-8 pt-12 '> 
-                        <div className='flex place-items-center  text-center gap-5'>
-                            <MdLocationPin className='w-10 h-16 mt-4 ' />
-                            <h2 className="mt-4  text-4xl">Edificio STEM</h2>
-                        </div>
-                        <h2 className="mt-10 text-2xl ml-3">Campus Gustavo Galindo</h2>
-                        <h2 className="mt-2 text-2xl  ml-3">Km. 30 vía Perimetral</h2>
+                <div className="container mx-auto flex flex-col place-content-center place-items-center select-none">
+                    <div className="">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.026445702663!2d-79.96903302423073!3d-2.1435536371656436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x902d7302f96af647%3A0x5a1dd75c545cedd!2sSTEM%20(Edificio%20de%20Posgrados)!5e0!3m2!1ses!2sus!4v1717983642893!5m2!1ses!2sus"
+                            style={{ border: 0 }}
+                            allowFullScreen={true}
+                            loading="lazy"
+                            referrerPolicy="no-referrer-when-downgrade"
+                            className="!w-80 h-48 max-w-full shadow-md rounded-lg"
+                        ></iframe>
+                    </div>
+                    <div className="mt-4">
+                        <h2 className="text-xl">Edificio STEM</h2>
+                        <h2 className="text-xl">Campus Gustavo Galindo</h2>
+                        <h2 className="text-xl">Km. 30 vía Perimetral</h2>
                     </div>
                 </div>
             </div>
--- src/pages/Home/Inicio.jsx
+++ src/pages/Home/Inicio.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Carrusel from "./Components/Carrusel";
 import { Infos } from "./Components/Infos";
-import Sponsors from "./Components/Sponsors"; 
+import Sponsors from "./Components/Sponsors";
 import Ubicacion from "./Components/Ubicacion";
 import CarruselConferences from "./Components/CarruselConferences";
 import AnimatedSection from "./../../shared/AnimatedSection";
@@ -9,42 +9,45 @@ import AnimatedSection from "./../../shared/AnimatedSection";
 function Inicio() {
     return (
         <>
-            <Carrusel/>
+            <Carrusel />
             <AnimatedSection>
                 <section className="select-none">
-                    <div className="mt-[3vw] mb-[10vw]" >
+                    <div className="mt-[3vw] mb-[10vw]">
                         <div className="mt-5 max-md:mb-[2vw] md:mb-[3em]">
-                            <h1 className="w-full text-center text-[6vw] md:text-[4vw] 2xl:text-[3vw] min-[2474px]:text-[2.25vw] font-sans font-bold" style={{color: '#004029'}}>
+                            <h1 className="w-full text-center text-[6vw] md:text-[4vw] 2xl:text-[3vw] min-[2474px]:text-[2.25vw] font-sans font-bold" style={{ color: '#004029' }}>
                                 CONFERENCIA
                             </h1>
                         </div>
                         <div className="flex place-content-center place-items-center">
-                            <Infos/>
+                            <Infos />
                         </div>
                     </div>
                 </section>
             </AnimatedSection>
             <AnimatedSection>
-                <Ubicacion/>
+                <div className='place-content-center place-items-center'>
+                    <Ubicacion />
+                </div>
+
             </AnimatedSection>
             <AnimatedSection>
                 <div className="flex flex-col place-content-center place-items-center select-none">
                     <h1 className="font-bold text-5xl">
                         CRONOGRAMA
                     </h1>
-                    <h2 className="w-full text-center text-[3.5vw] md:text-[2vw] 2xl:text-[1.5vw] min-[2474]:text-[1vw] font-mono p-5 font-semibold ">
-                        09:00 am - 13:00 pm 
+                    <h2 className="w-full text-center text-[3.5vw] md:text-[2vw] 2xl:text-[1.5vw] min-[2474px]:text-[1vw] font-mono p-5 font-semibold ">
+                        09:00 am - 13:00 pm
                     </h2>
-                    <h2 className="w-full text-center text-[3.5vw] md:text-[2vw] 2xl:text-[1.5vw] min-[2474]:text-[1vw] font-mono p-5 font-semibold ">
+                    <h2 className="w-full text-center text-[3.5vw] md:text-[2vw] 2xl:text-[1.5vw] min-[2474px]:text-[1vw] font-mono p-5 font-semibold ">
                         {"MUY PRONTO !!"}
                     </h2>
                 </div>
             </AnimatedSection>
             <AnimatedSection>
-                <CarruselConferences/>
+                <CarruselConferences />
             </AnimatedSection>
             <AnimatedSection>
-                <Sponsors/>
+                <Sponsors />
             </AnimatedSection>
         </>
     );
