66e8c190197103fbbeed76edf3ec726abe78cecc
aszurita <nzuritag@fiec.espol.edu.ec>
2024-06-16 00:04:14 -0500

    Revert "use instagram API manually to render posts + update pnpm yaml"
    This reverts commit 327ca1723ab8ceab1d9d5e69c6aa00ac6221a497.

--- src/pages/Home/Components/Posts.jsx
+++ src/pages/Home/Components/Posts.jsx
@@ -1,33 +1,7 @@
 import {Instagrams} from "./../../../data/Instagrams";
 import CardPosts from './CardPost';
 import ProfileCard from "./ProfileCard";
-import { useState, useEffect } from "react";
-
-const ACCESS_TOKEN = 'IGQWRNaXpCNE1MSVBnZA0pLNFVMSU9LQktEMlJEN3ZALVEE4R0ZAlWEVNcjV6b1dJLTE4MnZA5WnAxRGZAIcmM3NnhsejFuWDRpWHJUN2ljWHJBNUJJNnRpNGFRbjJmU2dqT19OQ2JkRGc0OEVMQQZDZD';
-
-const getPosts = async () => {
-  const URL = `https://graph.instagram.com/me/media?fields=permalink&access_token=${ACCESS_TOKEN}`;
-  const reqOptions = {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  }
-
-  const response = await fetch(URL, reqOptions);
-  const data = await response.json();
-
-  return data.data;
-}
-
 export default function Posts() {
-  const [posts, setPosts] = useState(null);
-
-  useEffect(() => {
-    getPosts().then((data) => {
-      setPosts(data.slice(0, 8));
-    });
-  }, []);
 
   return (
     <>
@@ -37,9 +11,9 @@ export default function Posts() {
         </div>
         <div className='flex justify-center items-center flex-wrap min-w-[360px] gap-5 mb-20'>
           {
-             posts && posts.map(({ permalink },index)=>(
-              <CardPosts  key={index} post={permalink}/>
-            )) 
+            Instagrams.publications.map((post,index)=>(
+              <CardPosts  key={index} post={post}/>
+            ))
           }
         </div>
       </div>
