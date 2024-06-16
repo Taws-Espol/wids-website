4d41ca8444ce31cc3262b910e3bdddfa39b743a3
Jose Luis <jchongac@fiec.espol.edu.ec>
2024-06-16 02:50:33 -0500

    Carrusel responsive

--- src/pages/Home/Components/Carrusel.jsx
+++ src/pages/Home/Components/Carrusel.jsx
@@ -35,18 +35,18 @@ export default function Carrusel() {
             <div className='hidden group-hover:block  group-hover:z-20 absolute top-[50%] translate-x-0 translate-y-[-50%] right-5 text-2xl rounded-full p-2 bg-black/20 text-white cursor-pointer'>
                 <BsChevronCompactRight onClick={nextImage} size={30} />
             </div>
-            <div className="absolute max-sm:right-5 xl:bottom-[50px] max-xl:bottom-[22px] 2xl:right-10 max-xl:right-[20%] flex justify-around">
+            <div className="xl:absolute place-content-center relative bottom-[20vw] sm:bottom-[17vw] md:bottom-[16vw] xl:bottom-[50px] xl:right-10 flex">
                 <Timer key={currentIndex} Event_date = {new Date(infoImages[currentIndex].dateTimer)}/>
             </div>
             <div className="absolute top-0 left-0 w-full h-full overflow-hidden group-hover:z-10">
-                <div className="absolute top-[-50%] left-[-1200px] text-white text-shadow-black select-none font-acumin rounded-full h-[1900px] w-[1900px] overflow-hidden bg-primary-yellow max-xl:h-full max-xl:w-full max-xl:top-[-65%] max-xl:left-[-30%]">
+                <div className="absolute xl:top-[-50%] xl:left-[-1220px] text-white text-shadow-black select-none font-acumin rounded-full xl:h-[1900px] xl:w-[1900px] overflow-hidden bg-primary-yellow h-full w-full top-[-78%] xsm:top-[-74%] sm:top-[-70%] left-[-45%] md:top-[-62%] lg:top-[-56%] md:left-[-45%]">
                 </div>
             </div>
-            <div className="absolute top-[40%] left-[5%] text-primary-dark-green text-shadow-black select-none font-acumin group-hover:z-20 flex flex-col gap-5 max-xl:top-10 max-xl:left-10 max-lg:text-center">
-                <p className="text-7xl font-acumin font-bold max-sm:text-2xl max-xl:text-3xl">{infoImages[currentIndex].title}</p>
-                <p className="text-[3vh] font-acumin font-normal ">{infoImages[currentIndex].date}</p>
-                <p className="text-[3vh] font-acumin font-normal ">{infoImages[currentIndex].place}</p>
-                <p className="text-[3vh] font-acumin font-normal ">{infoImages[currentIndex].uni}</p>
+            <div className="absolute top-[35%] left-[5vw] 5xl:left-[3%] text-primary-dark-green text-shadow-black select-none font-acumin group-hover:z-20 flex flex-col max-xl:gap-[1.5vw] gap-4 max-xl:top-10 max-xl:left-15">
+                <p className="text-[7vw] xsm:text-[6.5vw] sm:text-[6vw] xl:text-[5.5em] 2xl:text-[6em] 5xl:text-[5em] font-acumin font-bold">{infoImages[currentIndex].title}</p>
+                <p className="text-[4vw] xsm:text-[3.5vw] sm:text-[3vw] xl:text-[2.5em] 2xl:text-[3em] 5xl:text-[3em] font-acumin font-normal ">{infoImages[currentIndex].date}</p>
+                <p className="text-[4vw] xsm:text-[3.5vw] sm:text-[3vw] xl:text-[2.5em] 2xl:text-[3em] 5xl:text-[3em] font-acumin font-normal ">{infoImages[currentIndex].place}</p>
+                <p className="text-[4vw] xsm:text-[3.5vw] sm:text-[3vw] xl:text-[2.5em] 2xl:text-[3em] 5xl:text-[3em] font-acumin font-normal ">{infoImages[currentIndex].uni}</p>
                 <div className={`${infoImages[currentIndex].linkform ? 'block':'hidden'}`}>
                 <button className="text-white text-5xl max-sm:text-2xl max-sm:w-32 bg-primary-dark-green select-none rounded-3xl py-3 text-center w-2/3 font-acumin font-medium"
                 onClick={() => window.open(infoImages[currentIndex].linkform, '_blank')}
--- src/pages/Home/Components/Ubicacion.jsx
+++ src/pages/Home/Components/Ubicacion.jsx
@@ -1,8 +1,8 @@
 import { MdLocationPin } from "react-icons/md";
 const Ubicacion = () => {
     return (
-        <div className="md:pb-3 pb-[7vw] pt-[3.5vw] md:pl-3 md:pt-3 bg-neutral-grey bg-opacity-20 text-primary-dark-green flex flex-col md:flex-row items-center select-none !w-full">
-            <div className="flex !w-3/4">
+        <div className="md:pb-3 pb-[10vw] pt-[3.5vw] md:pl-3 md:pt-3 bg-neutral-grey bg-opacity-20 text-primary-dark-green flex flex-col md:flex-row items-center select-none !w-full">
+            <div className="flex w-full md:!w-3/4">
                 <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3987.026445702663!2d-79.96903302423073!3d-2.1435536371656436!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x902d7302f96af647%3A0x5a1dd75c545cedd!2sSTEM%20(Edificio%20de%20Posgrados)!5e0!3m2!1ses-419!2sec!4v1718137962522!5m2!1ses-419!2sec"
                     width="800"
@@ -11,7 +11,7 @@ const Ubicacion = () => {
                     allowFullScreen
                     loading="lazy"
                     referrerPolicy="no-referrer-when-downgrade"
-                    className="!w-full !h-[500px] shadow-md rounded-lg"
+                    className="max-md:mx-5 md:!w-full h-[500px] shadow-md rounded-lg"
                 ></iframe>
             </div>
             <div className="pr-[1vw] min-[1651px]:pl-[1.2vw] 5xl:pl-[2vw] flex flex-col">
@@ -21,7 +21,7 @@ const Ubicacion = () => {
                         <MdLocationPin className='w-[5vw] md:w-[3vw] lg:w-[2.5vw] 5xl:w-[3.8em] h-fit md:h-16 mt-[3vw] md:mt-4 ' />
                         <h2 className="mt-[3vw] text-[5vw] md:mt-4 md:text-[3vw] lg:text-[2.5vw] 5xl:text-[3.8em]">Edificio STEM</h2>
                     </div>
-                    <div className="">
+                    <div className="flex flex-col max-md:place-items-center max-md:mr-[1vw]">
                         <h2 className="text-[3vw] mt-[2vw] 5xl:mt-10 md:text-[2vw] lg:text-[1.5vw] 5xl:text-[2.2em] ml-3">Campus Gustavo Galindo</h2>
                         <h2 className="text-[3vw] mt-2 lg:text-[1.5vw] 5xl:text-[2.2em] md:text-[2vw]  ml-3">Km. 30 vía Perimetral</h2>
                     </div>
