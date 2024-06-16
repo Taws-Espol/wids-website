d25ffe7ffdd7753d6dac1ce6818ffd518acab59d
JuanFcoFernandez <juaffern@espol.edu.ec>
2024-06-16 12:34:00 -0500

    [FIX] Mejoras en el Hero, alineacion del CTA

--- src/pages/Eventos/Datathon/Components/Hero.jsx
+++ src/pages/Eventos/Datathon/Components/Hero.jsx
@@ -4,27 +4,25 @@ export default function Hero({ title, link }) {
   return (
     <AnimatedSection>
       <div
-        className="relative flex flex-col md:flex-row md:justify-between
-        justify-center items-center place-i md:items-start  bg-yellow-500 text-primary-dark-green
-         pb-10 overflow-hidden"
+        className="relative flex flex-col md:flex-row justify-center items-center 
+          md:items-start md:justify-start  bg-yellow-500 text-primary-dark-green
+          pb-10 overflow-hidden"
       >
-        <div className="w-4/5 md:w-1/2 flex flex-col justify-items-center justify-center py-10 md:p-16">
+        <div
+          className="w-4/5 md:w-1/2 flex flex-col  
+            items-center justify-center md:justify-start py-10 md:p-16"
+        >
           <h2
-            className="text-4xl md:text-5xl lg:text-6xl xl:text-7xl
+            className="text-4xl lg:text-5xl xl:text-6xl
             2xl:text-[80px]  text-center md:text-start  font-bold pb-10"
           >
             {title}
           </h2>
-          <div
-            className="flex flex-row max-md:flex-col place-content-center w-full 
-            text-2xl gap-10 px-20 max-md:m-0  max-sm:gap-5
-            max-md:gap-10 max-sm:text-lg max-md:text-xl justify-center
-            items-center lg:pl-56"
-          >
+          <div className="flex text-lg  justify-start">
             <button
               className="text-white bg-green-900 whitespace-nowrap select-none
                 rounded-3xl px-20 py-5 text-center font-acumin font-medium
-                max-md:text-lg max-md:px-4 text-3xl"
+                max-md:text-lg  text-3xl"
               onClick={() => window.open(link, "_blank")}
             >
               Registrate aquí
