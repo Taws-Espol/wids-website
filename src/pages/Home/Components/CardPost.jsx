be71336dc7d84159a687ec18a3662bc55a6c1793
aszurita <nzuritag@fiec.espol.edu.ec>
2024-06-16 12:16:13 -0500

    API Instagram Fix Local

--- src/pages/Home/Components/CardPost.jsx
+++ src/pages/Home/Components/CardPost.jsx
@@ -3,7 +3,7 @@ export default function CardPosts({post}) {
     return (
         <div className="flex justify-center overflow-hidden h-[330px] border-2 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:shadow-2xl">
             <div className="-mt-16 mb-0 py-0">
-                <InstagramEmbed url={post.url} width={350} height={400}/>
+                <InstagramEmbed url={post} width={350} height={400}/>
             </div>
         </div>
     )
