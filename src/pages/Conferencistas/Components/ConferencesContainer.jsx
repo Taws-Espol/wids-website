6a4c8440bcc32c27601a28025e536f65c19d5a5e
Luis Romero <lromeror@fiec.espol.edu.ec>
2024-06-28 00:30:55 -0500

    Scroll cambio

--- src/pages/Conferencistas/Components/ConferencesContainer.jsx
+++ src/pages/Conferencistas/Components/ConferencesContainer.jsx
@@ -1,16 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { ProfileCard } from "./../../Edicion/Components/ProfileCard";
 import { Tooltip } from "@material-tailwind/react";
 import { FaLinkedin } from "react-icons/fa";
 import { FaXTwitter } from "react-icons/fa6";
 
-export function ConferenceContainer({ edicionData, campo= "conferencistas" }) {
-    const [active, setActive] = useState('Cronograma');
+export function ConferenceContainer({ edicionData, campo = "conferencistas" }) {
     const [conferenceActive, setConferenceActive] = useState(null);
+    const [active, setActive] = useState('Cronograma');
+    const detailsDivRef = useRef(null); 
 
     const changeConferenceActive = (conferencista) => {
         setConferenceActive(conferencista);
         setTimeout(() => {
+            if (detailsDivRef.current) {
+                detailsDivRef.current.scrollTop = 0; 
+            }
             document.getElementById("details-div").scrollIntoView({ behavior: 'smooth' });
         }, 0);
     };
@@ -32,13 +36,13 @@ export function ConferenceContainer({ edicionData, campo= "conferencistas" }) {
             <div className="font-acumin flex flex-row place-content-center place-items-center gap-10 flex-wrap">
                 {conferenceActive && (
                     <div id="details-div" className="flex flex-row flex-wrap place-content-center place-items-center w-full gap-10 my-20 py-10 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 hover:shadow-2xl">
-                        <img src={image} alt="Conferencista" className="h-96 w-96  max-sm:w-80 max-sm:h-80 object-cover rounded-3xl" />
+                        <img src={image} alt="Conferencista" className="h-96 w-96 max-sm:w-80 max-sm:h-80 object-cover rounded-3xl" />
                         <div className="flex flex-col h-[384px] w-[800px] max-sm:w-[350px] justify-center align-middle rounded-xl p-10 gap-8">
                             <div className="flex flex-col max-sm:mt-10">
                                 <p className="text-primary-dark-green font-bold text-3xl">{conferenceActive.name}</p>
                                 <p className="text-2xl">{conferenceActive.work}, {conferenceActive.place}</p>
                             </div>
-                            <div className="flex place-content-start w-9/10 min-h-[200px] overflow-scroll">
+                            <div className="flex place-content-start w-9/10 min-h-[200px] overflow-scroll" ref={detailsDivRef}>  {}
                                 <p className="text-justify">
                                     {conferenceActive.info}
                                 </p>
