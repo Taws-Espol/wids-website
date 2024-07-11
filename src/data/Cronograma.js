83f53facc8850ecf21a6cb0ce23b8e63b98a00bf
Jose Luis <jchongac@fiec.espol.edu.ec>
2024-07-11 14:50:48 -0500

    Cronograma arreglado

--- src/data/Cronograma.js
+++ src/data/Cronograma.js
@@ -1,43 +1,103 @@
 export let cronograma = {
     Conferencias : [
         {
-            type:'Conferencia',
+            color:'verde',
             title:'Bienvenida',
-            info:'Cecilia Paredes, Ph.D., Rectora de ESPOL',
+            info:'Carlos Monsalve, Ph.D., Vice Rector de ESPOL',
+            image:'',
             time: '09:00 AM',
-            day: 'Día 1'
         },
         {
-            type:'Break',
-            title:'Charla 1',
-            info:' ',
-            time: '09:30 AM'
+            color:'morado',
+            title:'Transformando Empresas con datos e insights',
+            info:'Ysabel Atiencia, Ing., Analista de Datos, AB InBev',
+            image:'YsabelAtiencia.png',
+            time: '09:20 AM'
         },
         {
-            type:'Conferencia',
-            title:'Charla 2',
-            info:' ',
+            color:'celeste',
+            title:'Importancia de Model Governance en la era de la Ciecia de Datos',
+            info:'Valeria Valencia, Mgtr., Gobierno de Información y Analítica - Subgerente Model Governance, Banco Guayaquil',
+            image:'ValeriaValenciaRenteria.webp',
+            time: '09:40 AM'
+        },
+        {
+            color:'verde',
+            title:'MRI Segmentation of Brainstem Structures using Deep Learning Techniques',
+            info:'Seoyoung Oh, M.Sc.,Candidata Ph.D., Sorbonne University',
+            image:'SeoyoungOh.png',
             time: '10:00 AM'
         },
         {
-            type:'Conferencia',
-            title:'Charla 3',
-            info:' ',
-            time: '11:00 AM'
+            color:'morado',
+            title:'Optimizando el Análisis de Datos: Cómo la Segmentación de Usuarios Potencia el Entendimiento Profundo',
+            info:'Viviana Córdova, Lic., Líder de conversión y análisis, Rappi',
+            image:'VivianaCordova.webp',
+            time: '10:25 AM'
         },
         {
-            type:'Conferencia',
-            title:'Charla 4',
-            info:' ',
-            time: '12:00 AM'
+            color:'celeste',
+            title:'De las Olas a los Datos: Cómo la Ciencia de Datos Revolucionó mi Vida Profesional',
+            info:'Kelly Mendoza, Ing., Científica de Datos, ESPOL',
+            image:'KellyMendoza.webp',
+            time: '10:45 AM'
         },
         {
-            type:'Conferencia',
-            title:'Charla 5',
-            info:' ',
-            time: '13:00 AM'
+            color:'verde',
+            title:'AI, Academia and Industry: Bridging the Gap Between Theory and Practice',
+            info:'Mirian Silva, M.Sc., AI Engineer, IBM Research/UFMG',
+            image:'Mirian Silva.webp',
+            time: '11:05 AM'
         },
-
+        {
+            color:'morado',
+            title:'Jóvenes Ecuatorianos y el Mercado de Valores: Desafíos y Oportunidades',
+            info:'Cindy Cunalata, Econ., Analista de Riesgos, Pycca',
+            image:'CindyCunalata.webp',
+            time: '11:30 AM'
+        },
+        {
+            color:'celeste',
+            title:'Aplicación de Ciencia de Datos en Agricultura de Precisión: Caso Banano Ecuatoriano',
+            info:'Daniela Roldán, Econ. y M.Sc., Graduate Management Trainee, AB InBev',
+            image:'Daniela roldan.webp',
+            time: '11:50 AM'
+        },
+        {
+            color:'verde',
+            title:'Licitaciones Transparentes: El Poder del Análisis de Datos para Prevenir Irregularidades',
+            info:'María Belén Rojas, Econ., Analista de Datos, Banco Bolivariano',
+            image:'MaríaBelénRojas.webp',
+            time: '12:10 AM'
+        },
+        {
+            color:'morado',
+            title:'SciDiff: Modelo de difusión multi-escala para la generación de trayectorias Lagrangianas',
+            info:'María Belén Guaranda, M.Sc., LISN, Universidad Paris-Saclay',
+            image:'MariaBelenGuaranda.webp',
+            time: '12:35 AM'
+        },
+        {
+            color:'celeste',
+            title:'Eficiencia Gubernamental: Validación con Reconocimiento de Imágenes',
+            info:'Nicole Gaibor, Econ., Gobierno',
+            image:'NicoleGaibor.webp',
+            time: '12:55 AM'
+        },
+        {
+            color:'verde',
+            title:'Revolutionizing Data Centers: The Convergence of Data Science and Hardware',
+            info:'María Isabel Mera, Ph.H., Profesora e Investigadora de FIEC, ESPOL',
+            image:'MariaIsabelMera.webp',
+            time: '13:15 AM'
+        },
+        {
+            color:'morado',
+            title:'Clausura',
+            info:'Carmen Vaca, Ph.D., Profesora de FIEC, ESPOL',
+            image:'carmenVacaRuiz.webp',
+            time: '13:35 AM'
+        }
     ],
     Talleres : [
         {
--- src/pages/Cronograma/Components/ItemTimeLine.jsx
+++ src/pages/Cronograma/Components/ItemTimeLine.jsx
@@ -1,20 +1,21 @@
+/* eslint-disable react/prop-types */
 import { VerticalTimelineElement } from 'react-vertical-timeline-component';
 import 'react-vertical-timeline-component/style.min.css';
 export function ItemTimeLine({ Evento }) {
-
+    const imagenConferencista = "/assets/Eventos/Ediciones/2024/Images/conferencistas/" + Evento.image
     let imagen = "";
     let background_color = "";
-    let value = Evento.type;
+    let value = Evento.color;
     switch (value) {
-        case "Conferencia":
+        case "verde":
             imagen = "/assets/images-cardInfo/Conferencia.webp";
             background_color = "#4ab969";
             break;
-        case "Break":
+        case "morado":
             imagen = "/assets/images-cardInfo/Break.webp";
             background_color = "#a2a2ff";
             break;
-        case "Taller":
+        case "celeste":
             imagen = "/assets/images-cardInfo/Conferencia.webp";
             background_color = "#00a7d5";
             break;
@@ -41,10 +42,18 @@ export function ItemTimeLine({ Evento }) {
                         <p key={index} className="!text-lg">{infoItem}</p>
                     ))
                 ) : (
-                    <p className="">{Evento.info}</p>
-                )}
+                        <div className='h-[18.5em]'>
+                            <br/>
+                            <div className='p-4 flex items-center justify-center'>
+                                <img src={imagenConferencista} alt='conferencista' className='h-[12rem] w-[12rem] object-cover rounded-full  ' />
+                            </div>
+                            <div className=' h-100%'>
+                                <p className='align-bottom'>{Evento.info}</p>
+                            </div>
+                        </div>
+                    )}
                 {Evento.time && (
-                    <div className="flex flex-row items-center">
+                    <div className="mt-2 flex flex-row items-center">
                         <img src="/assets/Eventos/Datathon/reloj.webp" alt="Logo" className="w-8 h-8  mt-[21px] mr-2" />
                         <p className='!text-2xl'>{Evento.time}</p>
                     </div>
