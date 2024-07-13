d6be8dccd5a413f31465d1d6d4efb9a8fddec5b6
Jose Luis <jchongac@fiec.espol.edu.ec>
2024-07-13 09:09:52 -0500

    Fix cronograma

--- src/pages/Cronograma/Cronograma.jsx
+++ src/pages/Cronograma/Cronograma.jsx
@@ -13,9 +13,6 @@ function Cronograma() {
     return (
         <div className="flex flex-col place-content-center place-items-center gap-10 select-none">
             <h1 className="text-4xl text-primary-dark-green font-acumin pt-5 font-bold">Cronograma</h1>
-            <div className="flex flex-wrap place-content-center  max-lg:gap-10 gap-4 px-4 pb-20 select-none">
-                <ButtonEditionComponents key='1' text='Conferencias' active={active} changeButton={() => changeButton('Conferencias')} />
-            </div>
             <div className='w-full'>
                 {<TimeLine Data={cronograma} active={active}/>}
             </div>
