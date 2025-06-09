9c940ff6e057009af483f82ff56d1ddc1c89fb04
Jonthz <jnzambra@fiec.edu.espol.ec>
2025-06-09 01:43:37 -0500

    avance madrugada

--- src/shared/Footer.jsx
+++ src/shared/Footer.jsx
@@ -34,42 +34,48 @@ function Footer() {
       <div
         className={`flex flex-wrap place-content-center place-items-center bg-neutral-grey bg-opacity-20 ${nothome ? 'hidden' : 'block'}`}
       >
-        <img src="/assets/WidsLogoVertical.webp" className="h-96 w-96" />
-        <div className="flex flex-col max-lg:place-content-center max-lg:place-items-center">
-          <h2 className="mb-3 font-acumin text-7xl font-bold text-primary-dark-green max-sm:text-5xl">
-            Contacto
-          </h2>
-          <h3 className="mb-12 !select-text font-acumin text-5xl text-primary-dark-green max-sm:text-3xl">
+        <img src="/assets/WidsLogoVertical.webp" className="h-48 w-48" />
+        <div className="mx-8 hidden h-40 border-l-2 border-[rgb(0,65,43)] md:block"></div>
+        <div className="flex flex-col items-center text-center md:items-start md:text-left">
+          <p className="mb-1 text-lg font-semibold text-[rgb(0,65,43)]">
+            Email
+          </p>
+          <a
+            href="mailto:wids_taws@fiec.espol.edu.ec"
+            className="text-xl font-bold text-primary-dark-green hover:underline md:text-2xl lg:text-2xl"
+          >
             wids_taws@fiec.espol.edu.ec
-          </h3>
+          </a>
+          <div className="mt-4 flex space-x-6">
+            <a href={redes.fb} target="_blank" rel="noopener noreferrer">
+              <img
+                src="/assets/footer/icons8-facebook.svg"
+                alt="Facebook"
+                className="h-10 w-10 text-blue-600 hover:text-blue-700 xl:h-12 xl:w-12"
+              />
+            </a>
+            <a href={redes.ig} target="_blank" rel="noopener noreferrer">
+              <img
+                src="/assets/footer/icons8-instagram.svg"
+                alt="Instagram"
+                className="h-10 w-10 xl:h-12 xl:w-12"
+              />
+            </a>
+            <a href={redes.tw} target="_blank" rel="noopener noreferrer">
+              <img
+                src="/assets/footer/icons8-twitterx.svg"
+                alt="X (anteriormente Twitter)"
+                className="h-8 w-8 xl:h-10 xl:w-10"
+              />
+            </a>
+          </div>
         </div>
       </div>
-      <ul className="bottom-0 flex w-full justify-center bg-[rgb(0,65,43)] pb-6 pt-4">
-        <li className="pl-5 pr-5">
-          <a href={redes.fb} target="_blank">
-            <img
-              src="/assets/footer/icons8-facebook.svg"
-              className="h-6 w-6 xl:h-10 xl:w-10"
-            />
-          </a>
-        </li>
-        <li className="pl-5 pr-5">
-          <a href={redes.ig} target="_blank">
-            <img
-              src="/assets/footer/icons8-instagram.svg"
-              className="h-6 w-6 xl:h-10 xl:w-10"
-            />
-          </a>
-        </li>
-        <li className="pl-5 pr-5">
-          <a href={redes.tw} target="_blank">
-            <img
-              src="/assets/footer/icons8-twitterx.svg"
-              className="h-6 w-6 xl:h-10 xl:w-10"
-            />
-          </a>
-        </li>
-      </ul>
+      <div className="mb-16 mt-8 flex h-2 w-full px-4 md:px-8 lg:px-16">
+        <div className="flex-1 bg-primary-yellow"></div>
+        <div className="flex-1 bg-primary-acc-green"></div>
+        <div className="flex-1 bg-primary-acc-blue"></div>
+      </div>
     </footer>
   );
 }
