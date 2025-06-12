f63b92fdcc70d1cdde1997291396b9c123454e4e
Luis F. Romero <lromeror@fiec.espol.edu.ec>
2025-06-12 08:13:45 -0500

    Ambrassador

---
+++ src/data/Ambrassador.js
@@ -0,0 +1,22 @@
+[
+  {
+    name: 'Daniela Roldón Carranza',
+    role: 'Coembajadora',
+    image: 'https://www.w3schools.com/w3images/avatar2.png',
+  },
+  {
+    name: 'Carlos Pérez',
+    role: 'Coembajador',
+    image: 'https://www.w3schools.com/w3images/avatar2.png',
+  },
+  {
+    name: 'María López',
+    role: 'Coembajadora',
+    image: 'https://www.w3schools.com/w3images/avatar2.png',
+  },
+  {
+    name: 'Juan Rodríguez',
+    role: 'Coembajador',
+    image: 'https://www.w3schools.com/w3images/avatar2.png',
+  },
+];
--- src/pages/Home/Components/Ambrassador.jsx
+++ src/pages/Home/Components/Ambrassador.jsx
@@ -1,52 +1,54 @@
+import { useState, useEffect } from 'react';
 import CardAmbrassador from './CardAmbrassador';
 
 const Ambassador = () => {
+  const [ambassadors, setAmbassadors] = useState([]);
+
+  useEffect(() => {
+    const fetchAmbassadors = async () => {
+      const response = await fetch('../../../data/Ambrassador.js');
+      const data = await response.json();
+      setAmbassadors(data);
+    };
+
+    fetchAmbassadors();
+  }, []);
+
   return (
     <div className="relative bg-cyan-200 p-8 text-center shadow-lg">
       <svg
-        width="150"
-        height="150"
-        className="absolute -top-16 right-40 hidden sm:block"
+        width="200"
+        height="200"
+        className="absolute -right-3 -top-16 hidden sm:block md:right-10 lg:right-28"
       >
-        <circle cx="75" cy="75" r="70" fill="#0891B2" />
+        <circle cx="100" cy="100" r="80" fill="#0891B2" />
         <text
-          x="75"
-          y="75"
+          x="100"
+          y="100"
           textAnchor="middle"
           dominantBaseline="middle"
           fill="white"
-          fontSize="16"
+          fontSize="23"
           fontWeight="600"
         >
           Team WiDS
         </text>
       </svg>
-      <h2 className="mb-6 pl-10 pr-10 text-left text-2xl font-semibold text-gray-700 sm:pl-40 sm:pr-20">
+
+      <h2 className="mx-auto mb-10 ml-4 max-w-xl px-6 text-left text-4xl font-semibold text-gray-700 sm:px-0 md:ml-6 lg:ml-[110px]">
         Estas son las voces que representan a WiDS como embajadora y
         coembajadoras.
       </h2>
 
       <div className="flex flex-wrap justify-center gap-10">
-        <CardAmbrassador
-          name="Daniela Roldón Carranza"
-          role="Coembajadora"
-          image="https://www.w3schools.com/w3images/avatar2.png"
-        />
-        <CardAmbrassador
-          name="Daniela Roldón Carranza"
-          role="Coembajadora"
-          image="https://www.w3schools.com/w3images/avatar2.png"
-        />
-        <CardAmbrassador
-          name="Daniela Roldón Carranza"
-          role="Coembajadora"
-          image="https://www.w3schools.com/w3images/avatar2.png"
-        />
-        <CardAmbrassador
-          name="Daniela Roldón Carranza"
-          role="Coembajadora"
-          image="https://www.w3schools.com/w3images/avatar2.png"
-        />
+        {ambassadors.map((ambassador, index) => (
+          <CardAmbrassador
+            key={index}
+            name={ambassador.name}
+            role={ambassador.role}
+            image={ambassador.image}
+          />
+        ))}
       </div>
     </div>
   );
