8182377fe4ccd2a2059667ed47d0e623537249b9
Luis F. Romero <lromeror@fiec.espol.edu.ec>
2025-06-11 23:58:36 -0500

    Cambios al nivel de ambrassador

--- src/pages/Home/Components/Ambrassador.jsx
+++ src/pages/Home/Components/Ambrassador.jsx
@@ -3,7 +3,11 @@ import CardAmbrassador from './CardAmbrassador';
 const Ambassador = () => {
   return (
     <div className="relative bg-cyan-200 p-8 text-center shadow-lg">
-      <svg width="150" height="150" className="absolute -top-16 right-40">
+      <svg
+        width="150"
+        height="150"
+        className="absolute -top-16 right-40 hidden sm:block"
+      >
         <circle cx="75" cy="75" r="70" fill="#0891B2" />
         <text
           x="75"
@@ -17,7 +21,7 @@ const Ambassador = () => {
           Team WiDS
         </text>
       </svg>
-      <h2 className="mb-6 pl-40 pr-20 text-left text-2xl font-semibold text-gray-700">
+      <h2 className="mb-6 pl-10 pr-10 text-left text-2xl font-semibold text-gray-700 sm:pl-40 sm:pr-20">
         Estas son las voces que representan a WiDS como embajadora y
         coembajadoras.
       </h2>
@@ -26,22 +30,22 @@ const Ambassador = () => {
         <CardAmbrassador
           name="Daniela Roldón Carranza"
           role="Coembajadora"
-          image="https://www.w3schools.com/w3images/avatar2.png" // Cambia esta URL por la que tengas
+          image="https://www.w3schools.com/w3images/avatar2.png"
         />
         <CardAmbrassador
           name="Daniela Roldón Carranza"
           role="Coembajadora"
-          image="https://www.w3schools.com/w3images/avatar2.png" // Cambia esta URL por la que tengas
+          image="https://www.w3schools.com/w3images/avatar2.png"
         />
         <CardAmbrassador
           name="Daniela Roldón Carranza"
           role="Coembajadora"
-          image="https://www.w3schools.com/w3images/avatar2.png" // Cambia esta URL por la que tengas
+          image="https://www.w3schools.com/w3images/avatar2.png"
         />
         <CardAmbrassador
           name="Daniela Roldón Carranza"
           role="Coembajadora"
-          image="https://www.w3schools.com/w3images/avatar2.png" // Cambia esta URL por la que tengas
+          image="https://www.w3schools.com/w3images/avatar2.png"
         />
       </div>
     </div>
