475247f759fecab5ed585d13c29d8cb9b3741cce
Luis F. Romero <127578885+lromeror@users.noreply.github.com>
2025-06-15 19:45:25 -0500

    76 ambassadors lr (#87)
    * Cambios Ambrassador
    
    * Cambios al nivel de ambrassador
    
    * Ambrassador
    
    * Update
    
    * remove unused variables
    
    ---------
    
    Co-authored-by: Braulio Rivas <brauliorivas2004@gmail.com>

--- src/pages/Home/Inicio.jsx
+++ src/pages/Home/Inicio.jsx
@@ -7,7 +7,7 @@ import CarruselConferences from './Components/CarruselConferences';
 import AnimatedSection from './../../shared/AnimatedSection';
 import Posts from './Components/Posts';
 import Programa from './Components/Programa';
-
+import Ambrassador from './Components/Ambrassador';
 function Inicio() {
   return (
     <>
@@ -60,6 +60,9 @@ function Inicio() {
 
       <AnimatedSection>
         <Sponsors />
+      </AnimatedSection>
+      <AnimatedSection>
+        <Ambrassador />
       </AnimatedSection>
     </>
   );
