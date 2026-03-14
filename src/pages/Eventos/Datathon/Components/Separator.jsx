b6cefb4a16248a94f7569bbbd87875cfea461d84
David Aragundy <waldaara@espol.edu.ec>
2026-03-14 13:06:27 -0500

    feat: initial commit

--- src/pages/Eventos/Datathon/Components/Separator.jsx
+++
@@ -1,9 +0,0 @@
-import { Component } from 'react';
-
-export default class Separator extends Component {
-  render() {
-    return (
-      <div className="mb-5 mt-5 h-[5px] w-32 rounded-full bg-gray-600 lg:mb-0 lg:mt-0 lg:h-32 lg:w-[5px]" />
-    );
-  }
-}
--- src/pages/Home/Components/Subtitle.jsx
+++
@@ -1,7 +0,0 @@
-export default function Subtitle({ text }) {
-  return (
-    <p className="font-acumin inline-block text-center text-3xl font-black text-green-950">
-      {text}
-    </p>
-  );
-}
